0874f304a148570cacdc35df2eb63ea82291432d
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-06-23 22:48:41 +0530

    ready event

---
+++ src/app/listeners/client/ready.js
@@ -0,0 +1,18 @@
+const { Listener } = require('discord-akairo');
+const Logger = require('../../utils/logger');
+
+class ReadyListener extends Listener {
+	constructor() {
+		super('ready', {
+			event: 'ready',
+			emitter: 'client',
+			category: 'client'
+		});
+	}
+
+	async exec() {
+		Logger.info(`${this.client.user.tag} (${this.client.user.id})`, { level: 'READY' });
+	}
+}
+
+module.exports = ReadyListener;
