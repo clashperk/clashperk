dc1628d34c8df9a082e5711658be15561320107a
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-06-27 13:57:30 +0530

    feat: profile commands

---
+++ src/app/commands/lookup/clanSearch.js
@@ -0,0 +1,75 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const qs = require('querystring');
+
+const STATUS = {
+	400: 'client provided incorrect parameters for the request.',
+	403: 'access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.',
+	404: 'invalid name, resource was not found.',
+	429: 'request was throttled, because amount of requests was above the threshold defined for the used API token.',
+	500: 'unknown error happened when handling the request.',
+	503: 'service is temprorarily unavailable because of maintenance.'
+};
+
+class ClanSearchCommand extends Command {
+	constructor() {
+		super('clan-search', {
+			aliases: ['clan-search'],
+			category: 'lookup',
+			description: {
+				content: 'Clash of Clans clan search command.',
+				usage: '<tag>',
+				examples: ['#2Q98URCGY', '2Q98URCGY']
+			},
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
+			args: [
+				{
+					id: 'name',
+					match: 'content',
+					prompt: {
+						start: 'what would you like to search for?',
+						retry: 'please provide a name to search clans!'
+					}
+				}
+			]
+		});
+	}
+
+	async exec(message, { name }) {
+		const query = qs.stringify({ name, limit: 10 });
+		const uri = `https://api.clashofclans.com/v1/clans?${query}`;
+		const res = await fetch(uri, { method: 'GET', headers: { Accept: 'application/json', authorization: `Bearer ${process.env.CLASH_API}` } });
+		const data = await res.json();
+
+		if (!res.ok) return message.util.reply(STATUS[res.status]);
+
+		const embed = new MessageEmbed()
+			.setColor(0x5970c1)
+			.setAuthor(`Showing Top ${data.items.length} Results`)
+			.setTitle(`Searching clans with name ${name}`);
+
+		for (const clan of data.items) {
+			let clan_type = '';
+			if (clan.type === 'inviteOnly') {
+				clan_type = 'Invite Only';
+			} else if (clan.type === 'closed') {
+				clan_type = 'Closed';
+			} else if (clan.type === 'open') {
+				clan_type = 'Open';
+			}
+
+			embed.addField(`${clan.name} (${clan.tag})`, [
+				`Level: ${clan.clanLevel}`,
+				`Members: ${clan.members}`,
+				`Points: ${clan.clanPoints}`,
+				`Status: ${clan_type} \\🏆 ${clan.requiredTrophies}`,
+				`${clan.location ? `Location: ${clan.location.name}` : ''}`
+			], true);
+		}
+
+		return message.util.send({ embed });
+	}
+}
+
+module.exports = ClanSearchCommand;
---
+++ src/app/commands/profile/link-clan.js
@@ -0,0 +1,58 @@
+const { Command } = require('discord-akairo');
+const Profile = require('../../models/Profile');
+
+class LinkClanCommand extends Command {
+	constructor() {
+		super('link-clan', {
+			aliases: ['link-clan', 'save-clan'],
+			category: 'profile',
+			channel: 'guild',
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS', 'ADD_REACTIONS'],
+			description: {
+				content: 'Saves a player to your discord account.',
+				usage: '<tag> [member]',
+				examples: ['#9Q92C8R20', '#9Q92C8R20 Suvajit']
+			},
+			args: [
+				{
+					id: 'data',
+					type: 'clan',
+					prompt: {
+						start: 'what would you like to search for?',
+						retry: (msg, { failure }) => failure.value
+					}
+				},
+				{
+					id: 'member',
+					type: 'member',
+					default: message => message.member
+				}
+			]
+		});
+	}
+
+	async exec(message, { data, member }) {
+		const profile = await Profile.findOne({
+			where: {
+				guild: member.guild.id,
+				user: member.id
+			}
+		});
+
+		if (profile) {
+			await profile.update({ clan_tag: data.tag, clan_name: data.name });
+			return message.util.send(`Successfully linked **${member.user.tag}** to *${data.name} (${data.tag})*`);
+		}
+
+		await Profile.create({
+			guild: message.guild.id,
+			user: member.id,
+			clan_tag: data.tag,
+			clan_name: data.name
+		});
+
+		return message.util.send(`Successfully linked **${member.user.tag}** to *${data.name} (${data.tag})*`);
+	}
+}
+
+module.exports = LinkClanCommand;
