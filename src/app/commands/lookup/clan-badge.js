0c6f01fa61e6e315de530d6ffe8f48c2005e11f9
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-04 20:29:57 +0530

    fix: api limitation

--- src/app/commands/lookup/clan-badge.js
+++ src/app/commands/lookup/clan-badge.js
@@ -30,6 +30,7 @@ class ClanBadgeCommand extends Command {
 			.setAuthor(`${data.name} (${data.tag})`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
 			.setColor(0x5970c1)
 			.setImage(data.badgeUrls.large);
+		if (data.clanLevel >= 21) embed.setFooter('clash of clans API does not provide actual image for level 21+ clans.');
 		return message.util.send({ embed });
 	}
 }
