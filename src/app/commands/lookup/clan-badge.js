9a44180e94e9e34fd4305872be3e7828d961f41b
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-04 20:21:48 +0530

    new: clan badge command

---
+++ src/app/commands/lookup/clan-badge.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+
+class ClanBadgeCommand extends Command {
+	constructor() {
+		super('clan-badge', {
+			aliases: ['clan-badge', 'badge'],
+			category: 'lookup',
+			description: {
+				content: 'Clash of Clans clan badge lookup command.',
+				usage: '<tag>',
+				examples: ['#2Q98URCGY', '2Q98URCGY']
+			},
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
+			args: [
+				{
+					id: 'data',
+					type: 'clan',
+					prompt: {
+						start: 'what would you like to search for?',
+						retry: (msg, { failure }) => failure.value
+					}
+				}
+			]
+		});
+	}
+
+	async exec(message, { data }) {
+		const embed = new MessageEmbed()
+			.setAuthor(`${data.name} (${data.tag})`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
+			.setColor(0x5970c1)
+			.setImage(data.badgeUrls.large);
+		return message.util.send({ embed });
+	}
+}
+
+module.exports = ClanBadgeCommand;
