e7b57ea2143540f1ec8dca3530a7315bbbbd44be
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-04 20:39:57 +0530

    fix: switched to description

--- src/app/commands/lookup/clan-badge.js
+++ src/app/commands/lookup/clan-badge.js
@@ -30,7 +30,11 @@ class ClanBadgeCommand extends Command {
 			.setAuthor(`${data.name} (${data.tag})`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
 			.setColor(0x5970c1)
 			.setImage(data.badgeUrls.large);
-		if (data.clanLevel >= 21) embed.setFooter('clash of clans api does not provide actual image for level 21+ clans');
+		if (data.clanLevel >= 21) {
+			embed.setDescription([
+				'*Clash of Clans API does not provide actual image for level 21+ clans. #Blame_Clash_of_Clans*'
+			]);
+		}
 		return message.util.send({ embed });
 	}
 }
