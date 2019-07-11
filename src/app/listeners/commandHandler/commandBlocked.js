a6e454d9f6b76906f073920e95edef0015fda065
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-11 10:59:49 +0530

    fix: beta notice

--- src/app/listeners/commandHandler/commandBlocked.js
+++ src/app/listeners/commandHandler/commandBlocked.js
@@ -14,7 +14,8 @@ class CommandBlockedListener extends Listener {
 		const text = {
 			owner: () => 'You must be the owner to use this command.',
 			guild: () => 'You must be in a guild to use this command.',
-			restrict: () => 'You can\'t use this command because you have been restricted.'
+			restrict: () => 'You can\'t use this command because you have been restricted.',
+			beta: () => 'Contact bot owner to use beta commands.'
 		}[reason];
 
 		const level = message.guild ? `${message.guild.name}/${message.author.tag}` : `${message.author.tag}`;
