c52b0c9ea45b0888e9b4f2078375562896871db4
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-11 22:39:26 +0530

    fix (event): message

---
+++ src/app/listeners/client/message.js
@@ -0,0 +1,19 @@
+const { Listener } = require('discord-akairo');
+
+class MessageListener extends Listener {
+	constructor() {
+		super('message', {
+			event: 'message',
+			emitter: 'client',
+			category: 'client'
+		});
+	}
+
+	async exec(message) {
+		if (message.mentions.has(this.client.user.id) && /^<@!?(\d+)>$/.test(message) && message.channel.permissionsFor(message.channel.guild.me).has(['SEND_MESSAGES'], false)) {
+			return message.channel.send(`**My prefix is \`${this.client.commandHandler.prefix(message)}\`\u200b**`);
+		}
+	}
+}
+
+module.exports = MessageListener;
