4b1b56feb7c200f370a7129e06755d2140093c2c
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-11 17:25:23 +0530

    test remove and fixed `members` commands

--- src/app/commands/lookup/mem-th.js
+++ src/app/commands/lookup/mem-th.js
@@ -1,7 +1,5 @@
 const { Command, Argument } = require('discord-akairo');
-const { Util } = require('discord.js');
 const fetch = require('node-fetch');
-const { stripIndent } = require('common-tags');
 
 const TownHallEmoji = {
 	2: '<:townhall2:534745498561806357>',
@@ -43,68 +41,44 @@ class MembersTHCommand extends Command {
 				}
 			]
 		});
+
+		this.cache = new Map();
 	}
 
 	async exec(message, { data, th }) {
-		const msg = await message.channel.send('**Making list of your clan members... <a:loading:538989228403458089>**');
-		let TH12 = '';
-		let TH11 = '';
-		let TH10 = '';
-		let TH09 = '';
-		let TH08 = '';
-		let TH07 = '';
-		let TH06 = '';
-		let TH05 = '';
-		let TH04 = '';
-		let TH03 = '';
-		let TH02 = '';
-		let TH01 = '';
+		await message.util.send('**Making list of your clan members... <a:loading:538989228403458089>**');
 
+		this.cache.set(`${message.author.id}${data.tag}`, []);
+		const array = this.cache.get(`${message.author.id}${data.tag}`);
 		for (const tag of data.memberList.map(member => member.tag)) {
 			const uri = `https://api.clashofclans.com/v1/players/${encodeURIComponent(tag)}`;
 			const res = await fetch(uri, { method: 'GET', headers: { Accept: 'application/json', authorization: `Bearer ${process.env.CLASH_API}` } });
 			const member = await res.json();
-
-			const TownHAll = member.townHallLevel;
-
-			if ((!th || th === 12) && TownHAll === 12) TH12 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 11) && TownHAll === 11) TH11 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 10) && TownHAll === 10) TH10 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 9) && TownHAll === 9) TH09 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 8) && TownHAll === 8) TH08 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 7) && TownHAll === 7) TH07 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 6) && TownHAll === 6) TH06 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 5) && TownHAll === 5) TH05 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 4) && TownHAll === 4) TH04 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 3) && TownHAll === 3) TH03 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 2) && TownHAll === 2) TH02 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
-			if ((!th || th === 1) && TownHAll === 1) TH01 += `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}\n`;
+			array.push({ tag: member.tag, name: member.name, townHallLevel: member.townHallLevel });
 		}
 
-		const split = stripIndent`<:clans:534765878118449152> **${data.name} (${data.tag}) ~ ${data.members}/50**
-		\n${TH12}${TH11}${TH10}${TH09}${TH08}${TH07}${TH06}${TH05}${TH04}${TH03}${TH02}${TH01}`;
+		const filter = array.filter(arr => arr.townHallLevel === th);
+		const first = this.paginate(th ? filter : array, 0, 32);
+		const second = this.paginate(th ? filter : array, 32, 35);
+		const third = this.paginate(th ? filter : array, 35, 50);
 
-		const result = this.break(split);
-		await msg.delete();
-		const time = `*\u200b**Executed in ${((Date.now() - message.createdTimestamp) / 1000).toFixed(2)} sec**\u200b*`;
-		if (Array.isArray(result)) {
-			return result.map(async res => message.channel.send(result[0] === res ? time : '', {
-				embed: {
-					color: 0x5970c1,
-					description: res
-				}
-			}));
+		const embed = this.client.util.embed().setColor(0x5970c1)
+			.setAuthor(`${data.name} (${data.tag}) ~ ${data.members}/50`, data.badgeUrls.medium)
+			.setDescription(first.items.map(member => `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}`).join('\n'));
+		if ((th && filter.length > 32) || data.members > 32) {
+			embed.addField(second.items.map(member => `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}`).join('\n'), [
+				third.items.length ? third.items.map(member => `${TownHallEmoji[member.townHallLevel]} **${member.name}** ${member.tag}`).join('\n') : '\u200b'
+			]);
 		}
-		return message.channel.send(time, {
-			embed: {
-				color: 0x5970c1,
-				description: result
-			}
-		});
+
+		this.cache.delete(`${message.author.id}${data.tag}`);
+		return message.util.send(`*\u200b**Executed in ${((Date.now() - message.createdTimestamp) / 1000).toFixed(2)} sec**\u200b*`, { embed });
 	}
 
-	break(data) {
-		return Util.splitMessage(data, { maxLength: 1900 });
+	paginate(items, start, end) {
+		return {
+			items: items.slice(start, end)
+		};
 	}
 }
 
