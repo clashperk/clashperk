e0060f182043dd58c4eb3aad54c8c25367752e98
Suvajit <45002322+esuvajit@users.noreply.github.com>
2019-07-14 13:23:25 +0530

    fix: dm

--- src/app/listeners/client/message.js
+++ src/app/listeners/client/message.js
@@ -11,9 +11,10 @@ class MessageListener extends Listener {
 
 	async exec(message) {
 		if (message.mentions.has(this.client.user.id) && /^<@!?(\d+)>$/.test(message)) {
-			try {
+			if (message.guild && message.channel.permissionsFor(message.channel.guild.me).has(['SEND_MESSAGES'], false)) {
 				return message.channel.send(`**Current prefix for this guild is \`${this.client.commandHandler.prefix(message)}\`\u200b**`);
-			} catch {}
+			}
+			return message.channel.send(`**My prefix is \`${this.client.commandHandler.prefix(message)}\`\u200b**`);
 		}
 	}
 }
