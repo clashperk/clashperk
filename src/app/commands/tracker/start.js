000c37fc08ba276de040849d924f566739cd5fc5
Suvajit <suvajit.c@outlook.com>
2019-07-27 13:44:46 +0530

    fix: unordered argument

--- src/app/commands/tracker/start.js
+++ src/app/commands/tracker/start.js
@@ -12,41 +12,45 @@ class StartCommand extends Command {
 			clientPermissions: ['ADD_REACTIONS', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
 			description: {
 				content: 'Starts the donation tracker in a channel.',
-				usage: '<clan tag> [channel] [hex color]>',
-				examples: ['#8QU8J9LP', '#8QU8J9LP #tracker #5970C1']
-			},
-			args: [
-				{
-					id: 'data',
-					type: 'clan',
-					unordered: true,
-					prompt: {
-						start: 'what clan do you want to track donations?',
-						retry: (msg, { failure }) => failure.value
-					}
-				},
-				{
-					id: 'channel',
-					type: 'textChannel',
-					unordered: true,
-					default: message => message.channel
-				},
-				{
-					id: 'color',
-					type: 'color',
-					unordered: true,
-					default: '#5970C1'
-				}
-			]
+				usage: '<clan tag> [channel/hexColor] [hexColor/channel]',
+				examples: ['#8QU8J9LP', '#8QU8J9LP #tracker #5970C1', '#8QU8J9LP #5970C1 #tracker']
+			}
 		});
 	}
 
+	*args() {
+		const data = yield {
+			type: 'clan',
+			unordered: false,
+			prompt: {
+				start: 'what clan do you want to track donations?',
+				retry: (msg, { failure }) => failure.value
+			}
+		};
+
+		const channel = yield {
+			type: 'textChannel',
+			unordered: [1, 2],
+			default: message => message.channel
+		};
+
+		const color = yield {
+			type: 'color',
+			unordered: [1, 2],
+			default: '#5970C1'
+		};
+
+		return { data, channel, color };
+	}
+
 	async exec(message, { data, channel, color }) {
+		console.log({ data, channel: channel.name, color });
+		if (!data) return message.util.reply('invalid tag, resource was not found.');
 		const clans = await Clans.findAll({ where: { guild: message.guild.id } });
 		const limit = this.client.settings.get(message.guild, 'clanLimit', 10);
 		if (clans.length >= limit) {
 			return message.util.send([
-				`You are already tracking ${limit} clans on this server!`,
+				`You are already tracking ${clans.length} clans on this server!`,
 				`If you need more, please contact **${this.client.users.get(this.client.ownerID).tag}**`
 			]);
 		}
