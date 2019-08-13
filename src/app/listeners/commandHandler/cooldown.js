ae686d1c91b99bf739bdc4681cad7d964a5f9d35
Suvajit <suvajit.c@outlook.com>
2019-08-13 22:23:37 +0530

    new cooldown msg

--- src/app/listeners/commandHandler/cooldown.js
+++ src/app/listeners/commandHandler/cooldown.js
@@ -16,8 +16,20 @@ class CooldownListener extends Listener {
 		const level = message.guild ? `${message.guild.name}/${message.author.tag}` : `${message.author.tag}`;
 		Logger.log(`=> ${command.id} ~ ${time}`, { level });
 
+		const cooldown = typeof command.cooldown === 'function' ? command.cooldown(message) : command.cooldown;
+
 		if (message.guild ? message.channel.permissionsFor(this.client.user).has('SEND_MESSAGES') : true) {
-			return message.reply(`You can use that command again in ${time}.`);
+			const embed = this.client.util.embed()
+				.setAuthor('Slow it down!')
+				.setColor(0x5970c1)
+				.setFooter('ClashPerk Premium')
+				.setDescription([
+					`You'll be able to use this command again in **${time}**`,
+					`The default cooldown is ${ms(cooldown, { long: true })}, but [donators](https://www.patreon.com/bePatron?u=14584309) only need to wait 1 sec!`,
+					'',
+					'While you wait, go [vote us](https://discordbots.org/bot/526971716711350273/vote) and check out our [Patreon](https://www.patreon.com/bePatron?u=14584309)'
+				]);
+			return message.channel.send({ embed });
 		}
 	}
 }
