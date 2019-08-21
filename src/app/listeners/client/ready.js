b539c6145d4ab1b06b1232fb175b53b663e84f05
Suvajit <suvajit.c@outlook.com>
2019-08-21 14:21:00 +0530

    fix: typo

--- src/app/listeners/client/ready.js
+++ src/app/listeners/client/ready.js
@@ -13,7 +13,7 @@ class ReadyListener extends Listener {
 	async exec() {
 		Logger.info(`${this.client.user.tag} (${this.client.user.id})`, { level: 'READY' });
 
-		this.voter.init();
+		this.client.voter.init();
 
 		if (this.client.user.id === process.env.CLIENT_ID) {
 			this.client.firebase.init();
