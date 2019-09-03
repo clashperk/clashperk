b23ff6260d829a01e9accd721c9db50909f1aff4
Suvajit <suvajit.c@outlook.com>
2019-09-03 20:20:20 +0530

    fix cwl war

--- src/app/commands/cwl/cwl-war.js
+++ src/app/commands/cwl/cwl-war.js
@@ -1,5 +1,7 @@
 const { Command } = require('discord-akairo');
 const fetch = require('node-fetch');
+const moment = require('moment');
+const { oneLine } = require('common-tags');
 
 const TownHallEmoji = {
 	2: '<:townhall2:534745498561806357>',
@@ -53,15 +55,75 @@ class CwlWarComamnd extends Command {
 				.setThumbnail(data.badgeUrls.medium)
 				.setDescription('CLAN IS NOT IN CWL');
 			return message.util.send({ embed });
-        }
-        for (const tag of body.rounds.splice(-1).warTags) {
-            const res = await fetch(`https://api.clashofclans.com/v1/clanwarleagues/wars/${encodeURIComponent(tag)}`, {
-                method: 'GET', headers: { Accept: 'application/json', authorization: `Bearer ${process.env.CLASH_API}` }
-            });
-            const war = await res.json();
-            if ((war.clan && war.clan.tag !== data.tag) || (war.clan && war.opponent.tag !== data.tag)) continue;
-            console.log(war);
-        }
+		}
+
+		return this.rounds(message, body, embed);
+	}
+
+	async rounds(message, body, embed) {
+		for (const tag of body.rounds.filter(d => !d.warTags.includes('#0')).pop().warTags) {
+			const res = await fetch(`https://api.clashofclans.com/v1/clanwarleagues/wars/${encodeURIComponent(tag)}`, {
+				method: 'GET', headers: { Accept: 'application/json', authorization: `Bearer ${process.env.CLASH_API}` }
+			});
+			const data = await res.json();
+			if ((data.clan && data.clan.tag === data.tag) || (data.opponent && data.opponent.tag === data.tag)) {
+				new Date(moment(data.warStartTime).toDate()).getTime();
+				embed.addField('War Against', `${data.opponent.name} (${data.opponent.tag})`)
+					.addField('State', data.state)
+					.addField('Team Size', `${data.teamSize} vs ${data.teamSize}`)
+					.addField('Rosters', [
+						`${data.clan.name}`,
+						`${this.count(data.clan.members)}`,
+						'',
+						`${data.opponent.name}``${this.count(data.opponent.name)}`
+					]);
+			}
+		}
+		embed = this.client.util.embed(embed);
+		return message.util.send({ embed });
+	}
+
+	count(members) {
+		let TH12 = 0;
+		let TH11 = 0;
+		let TH10 = 0;
+		let TH09 = 0;
+		let TH08 = 0;
+		let TH07 = 0;
+		let TH06 = 0;
+		let TH05 = 0;
+		let TH04 = 0;
+		let TH03 = 0;
+		let TH02 = 0;
+		let TH01 = 0;
+		for (const member of members) {
+			const TownHAll = member.townHallLevel;
+			if (TownHAll === 12) TH12++;
+			if (TownHAll === 11) TH11++;
+			if (TownHAll === 10) TH10++;
+			if (TownHAll === 9) TH09++;
+			if (TownHAll === 8) TH08++;
+			if (TownHAll === 7) TH07++;
+			if (TownHAll === 6) TH06++;
+			if (TownHAll === 5) TH05++;
+			if (TownHAll === 4) TH04++;
+			if (TownHAll === 3) TH03++;
+			if (TownHAll === 2) TH02++;
+			if (TownHAll === 1) TH01++;
+		}
+		return oneLine`
+            ${TH12 > 0 ? `${TownHallEmoji[12]} ${TH12 < 10 ? `0${TH12}` : `${TH12} `} ` : ''}
+            ${TH11 > 0 ? `${TownHallEmoji[11]} ${TH11 < 10 ? `0${TH11}` : `${TH11}`} ` : ''}
+            ${TH10 > 0 ? `${TownHallEmoji[10]} ${TH10 < 10 ? `0${TH10}` : `${TH10}`} ` : ''}
+            ${TH09 > 0 ? `${TownHallEmoji[9]} ${TH09 < 10 ? `0${TH09}` : `${TH09}`} ` : ''}
+            ${TH08 > 0 ? `${TownHallEmoji[8]} ${TH08 < 10 ? `0${TH08}` : `${TH08}`} ` : ''}
+            ${TH07 > 0 ? `${TownHallEmoji[7]} ${TH07 < 10 ? `0${TH07}` : `${TH07}`} ` : ''}
+            ${TH06 > 0 ? `${TownHallEmoji[6]} ${TH06 < 10 ? `0${TH06}` : `${TH06}`} ` : ''}
+            ${TH05 > 0 ? `${TownHallEmoji[5]} ${TH05 < 10 ? `0${TH05}` : `${TH05}`} ` : ''}
+            ${TH04 > 0 ? `${TownHallEmoji[4]} ${TH04 < 10 ? `0${TH04}` : `${TH04}`} ` : ''}
+            ${TH03 > 0 ? `${TownHallEmoji[3]} ${TH03 < 10 ? `0${TH03}` : `${TH03}`} ` : ''}
+            ${TH02 > 0 ? `${TownHallEmoji[2]} ${TH02 < 10 ? `0${TH02}` : `${TH02}`} ` : ''}
+            ${TH01 > 0 ? `${TownHallEmoji[1]} ${TH01 < 10 ? `0${TH01}` : `${TH01}`} ` : ''}`;
 	}
 }
 
