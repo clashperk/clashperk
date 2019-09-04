71d5bfbfcde62a57812f4d4ab0a863edf8eaa66f
Suvajit <suvajit.c@outlook.com>
2019-09-04 19:51:43 +0530

    fix constants

--- src/app/util/constants.js
+++ src/app/util/constants.js
@@ -1,18 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 
+const status_code = {
+	504: 'Service is temprorarily unavailable.',
+	400: 'Client provided incorrect parameters for the request.',
+	403: 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.',
+	404: 'Invalid tag, resource was not found.',
+	429: 'Request was throttled, because amount of requests was above the threshold defined for the used API token.',
+	500: 'Unknown error happened when handling the request.',
+	503: 'Service is temprorarily unavailable because of maintenance.'
+};
+
 class Constatns {
 	static status(code) {
-		const message = {
-			504: 'Service is temprorarily unavailable.',
-			400: 'Client provided incorrect parameters for the request.',
-			403: 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.',
-			404: 'Invalid tag, resource was not found.',
-			429: 'Request was throttled, because amount of requests was above the threshold defined for the used API token.',
-			500: 'Unknown error happened when handling the request.',
-			503: 'Service is temprorarily unavailable because of maintenance.'
-		}[code];
-
-		return message;
+		return status_code[code];
 	}
 
 	static geterror(member, type) {
@@ -32,7 +32,7 @@ class Constatns {
 			.setAuthor('Error')
 			.setColor(0xf30c11)
 			.setDescription([
-				`${this.status(code)}`
+				`${status_code[code]}`
 			]);
 
 		return embed;
