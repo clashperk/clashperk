95148aa5ca23ebb22b5e7de415ed671374c02132
Suvajit <suvajit.me@gmail.com>
2019-11-04 14:21:14 +0530

    move args to 0

--- src/app/commands/lookup/mem-th.js
+++ src/app/commands/lookup/mem-th.js
@@ -45,6 +45,10 @@ class MembersTHCommand extends Command {
 	}
 
 	*args() {
+		const th = yield {
+			type: Argument.range('integer', 1, 12, true),
+			unordered: true
+		};
 		const data = yield {
 			type: async (msg, str) => {
 				const resolver = this.handler.resolver.type('guildMember')(msg, str || msg.member.id);
@@ -71,11 +75,6 @@ class MembersTHCommand extends Command {
 				start: 'what would you like to search for?',
 				retry: 'what would you like to search for?'
 			}
-		};
-
-		const th = yield {
-			type: Argument.range('integer', 1, 12, true),
-			unordered: true
 		};
 
 		return { data, th };
