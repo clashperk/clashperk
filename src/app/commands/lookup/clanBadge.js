d7a3120c0d0a9d5ea29824678d578c81a402a220
Suvajit <suvajit.me@gmail.com>
2020-02-05 15:42:39 +0530

    renamed few files

--- src/app/commands/lookup/clanBadge.js
+++ src/app/commands/lookup/clanBadge.js
@@ -58,11 +58,7 @@ class ClanBadgeCommand extends Command {
 			.setAuthor(`${data.name} (${data.tag})`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
 			.setColor(0x5970c1)
 			.setImage(data.badgeUrls.large);
-		if (data.clanLevel >= 21) {
-			embed.setDescription([
-				'*Clash of Clans API does not provide actual image for level 21+ clans. #Blame_Supercell*'
-			]);
-		}
+
 		return message.util.send({ embed });
 	}
 }
