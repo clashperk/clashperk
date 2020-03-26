bd129943bb0e4b8fcd461141ff71f1904af96387
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-03-26 20:25:38 +0530

    New MemberList

---
+++ src/app/commands/lookup/memLeaguenew.js
@@ -0,0 +1,74 @@
+const { Command, Flag } = require('discord-akairo');
+const fetch = require('../../struct/Fetch');
+const { firestore } = require('../../struct/Database');
+const { geterror, fetcherror, leagueEmojis } = require('../../util/constants');
+
+class MembersLeagueCommand extends Command {
+	constructor() {
+		super('members-league_', {
+			aliases: ['mem'],
+			category: 'owner',
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
+			description: {
+				content: 'Displays a list of clan members.',
+				usage: '<tag>',
+				examples: ['#2Q98URCGY', '2Q98URCGY']
+			}
+		});
+	}
+
+	*args() {
+		const data = yield {
+			type: async (msg, str) => {
+				const resolver = this.handler.resolver.type('guildMember')(msg, str || msg.member.id);
+				if (!resolver && !str) return null;
+				if (!resolver && str) {
+					return fetch.clan(str).then(data => {
+						if (data.status !== 200) return msg.util.send({ embed: fetcherror(data.status) }) && Flag.cancel();
+						return data;
+					});
+				}
+				const data = await firestore.collection('linked_accounts')
+					.doc(resolver.id)
+					.get()
+					.then(snap => snap.data());
+				if (!data) return msg.util.send({ embed: geterror(resolver, 'clan') }) && Flag.cancel();
+				if (!data.clan) return msg.util.send({ embed: geterror(resolver, 'clan') }) && Flag.cancel();
+				return fetch.clan(data.clan).then(data => {
+					if (data.status !== 200) return msg.util.send({ embed: fetcherror(data.status) }) && Flag.cancel();
+					return data;
+				});
+			},
+			prompt: {
+				start: 'what would you like to search for?',
+				retry: 'what would you like to search for?'
+			}
+		};
+		return { data };
+	}
+
+	cooldown(message) {
+		if (this.client.patron.users.get(message.author, 'patron', false) || this.client.voter.isVoter(message.author.id)) return 1000;
+		return 3000;
+	}
+
+	async exec(message, { data }) {
+		const embed = this.client.util.embed()
+			.setColor(0x5970c1)
+			.setAuthor(`${data.name} (${data.tag}) ~ ${data.members}/50`, data.badgeUrls.medium)
+			.setDescription([
+				data.memberList.slice(0, 30).map(member => `${member.name} ${this.indent(member.name, member.trophies.toString())}`)
+			]);
+		return message.util.send({ embed });
+	}
+
+	paginate(items, start, end) {
+		return { items: items.slice(start, end) };
+	}
+
+	indent(name, data) {
+		return data.padStart(15 - name.length, ' ');
+	}
+}
+
+module.exports = MembersLeagueCommand;
