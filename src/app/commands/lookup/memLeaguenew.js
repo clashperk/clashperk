aca37f376706c51d2d79908efcd4294044ab13a7
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-03-28 11:30:00 +0530

    Quick Fix

--- src/app/commands/lookup/memLeaguenew.js
+++ src/app/commands/lookup/memLeaguenew.js
@@ -76,7 +76,7 @@ class MembersLeagueCommand extends Command {
 				.map(member => {
 					const name = this.name(member.name);
 					const star = this.star(member.cwlStar.toString());
-					return `${TownHallEmoji[member.townHallLevel]} \`${star}\`\u200b \`${name}\``;
+					return `${TownHallEmoji[member.townHallLevel]} \`\u200b${star}\`\u200b \`${name}\``;
 				})
 				.join('\n')
 		]);
