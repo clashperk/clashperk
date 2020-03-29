d26acc49594ebfffcfe9a791c4d2a0bcbf5d9b47
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-03-29 20:37:01 +0530

    One more xD

--- src/app/commands/lookup/warWeight.js
+++ src/app/commands/lookup/warWeight.js
@@ -98,7 +98,7 @@ class WarWeightCommand extends Command {
 			'<:grandwarden:524939931303411722>',
 			'<:royal_champion:653967122166185995>'
 		];
-		const header = stripIndent(`${emojis[0]} \u200b \u200b \u200b ${emojis[1]} ${emojis[2]} ${emojis[3]}`);
+		const header = stripIndent(`${emojis[0]} \u200b \u200b \u200b ${emojis[1]} ${emojis[2]} ${emojis[3]} ${emojis[4]}`);
 		const pages = [
 			this.paginate(memberList, 0, 25)
 				.items.map(member => {
