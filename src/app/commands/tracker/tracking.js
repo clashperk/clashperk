a53068f39d93a91b39a1b74a1ccedc6684cff5fc
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-01 10:51:16 +0530

    Final Commit

--- src/app/commands/tracker/tracking.js
+++ src/app/commands/tracker/tracking.js
@@ -53,9 +53,9 @@ class TrackingCommand extends Command {
 						const donation_log = this.client.channels.cache.has(donationlog);
 						const memberlog_log = this.client.channels.cache.has(donationlog);
 						return stripIndents(
-							`${++index} » **${data.name} (${data.tag})**
-						**Donation Log** » ${donationlog ? `${donation_log ? `<#${donationlog}>` : '#deleted-channel'}` : 'Inactive'}
-						**Player Log** » ${memberlog ? `${memberlog_log ? `<#${memberlog}>` : '#deleted-channel'}` : 'Inactive'}`
+							`**${++index} » ${data.name} (${data.tag})**
+						Donation Log **»** ${donationlog ? `${donation_log ? `<#${donationlog}>` : '#deleted-channel'}` : 'Inactive'}
+						Player Log **»** ${memberlog ? `${memberlog_log ? `<#${memberlog}>` : '#deleted-channel'}` : 'Inactive'}`
 						);
 					}).join('\n\n')
 				]);
