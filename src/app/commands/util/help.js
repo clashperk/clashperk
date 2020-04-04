c0f51a208fc16583337fce36af3c94bcf5f2d2bd
Suvajit <suvajit.me@gmail.com>
2020-04-04 23:25:43 +0530

    Update help.js

--- src/app/commands/util/help.js
+++ src/app/commands/util/help.js
@@ -88,7 +88,7 @@ class HelpCommand extends Command {
 			if (title) embed.addField(title, `${category.filter(cmd => cmd.aliases.length > 0).map(cmd => `\`${prefix}${cmd.aliases[0].replace(/-/g, '')}\` - ${cmd.description.content.toLowerCase()}`).join('\n')}`);
 		}
 
-		return message.util.send('For more help please join the support server, link is below.', { embed });
+		return message.util.send('For more help please join the support server, link is below.\nhttps://discord.gg/ppuppun', { embed });
 	}
 }
 
