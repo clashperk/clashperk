b7c407b0e3afe048356e4369e908eed42c1cd09b
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-06 09:19:48 +0530

    BugTest

--- src/app/commands/cwl/CWLattacks.js
+++ src/app/commands/cwl/CWLattacks.js
@@ -157,6 +157,8 @@ class CwlAttacksComamnd extends Command {
 						missing += `\`${this.index(member.mapPosition)} ${star[member.attacks[0].stars]} ${this.percentage(member.attacks[0].destructionPercentage)}% ${this.padEnd(member.name)}\`\n`;
 					}
 
+					message.channel.send(missing.length);
+
 					embed.setDescription(`${missing || 'Nobody Attacked'}`);
 					embed.addField('War Ended', `${moment.duration(Date.now() - end).format('D [days], H [hours] m [mins]', { trim: 'both mid' })} ago`)
 						.addField('Stats', [
