3691bc85fd592fdfff8aeb53bc412bbd8042a58e
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-09 10:56:49 +0530

    Remove test

--- src/app/commands/tracker/lastonline.js
+++ src/app/commands/tracker/lastonline.js
@@ -70,7 +70,7 @@ class LastOnlineCommand extends Command {
 			.setDescription([
 				`\`\`\`\u200e${'Last On'.padStart(7, ' ')}   ${'Name'.padEnd(20, ' ')}\n${this.filter(data, clan)
 					.map(m => `${m.lastOnline ? require('ms')(m.lastOnline).padStart(7, ' ') : '55555'.padStart(7, ' ')}   ${this.padEnd(m.name)}`)
-					.join('\n')}\`\`\``.length
+					.join('\n')}\`\`\``
 			]);
 
 		return message.util.send({ embed });
