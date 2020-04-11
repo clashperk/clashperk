1fe509c22587d81a1b598057d46918104c4bf1c0
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-11 09:57:05 +0530

    remove clan badge from warlog command

--- src/app/commands/lookup/warlog.js
+++ src/app/commands/lookup/warlog.js
@@ -60,7 +60,7 @@ class WarlogCommand extends Command {
 	async exec(message, { data }) {
 		const embed = new MessageEmbed()
 			.setColor(0x5970c1)
-			.setAuthor(`${data.name} (${data.tag}) ↗`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
+			.setAuthor(`${data.name} (${data.tag})`, null, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
 			.setTitle(`${data.warWins} wins, ${data.isWarLogPublic ? `${data.warLosses} losses,` : ''} win streak ${data.warWinStreak}`)
 			.setThumbnail(data.badgeUrls.medium);
 
