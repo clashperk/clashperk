a9fe89703d1854382c5a6750fef27758565071fc
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-20 12:03:09 +0530

    fancy

--- src/app/commands/tracker/tracking.js
+++ src/app/commands/tracker/tracking.js
@@ -85,7 +85,7 @@ class TrackingCommand extends Command {
 					}).join('\n\n')
 				]);
 			}
-			embed.setFooter(`${data.length} ${data.length === 1 ? 'clan' : 'clans'}`);
+			embed.setFooter(`${data.length} ${data.length === 1 ? 'clan' : 'clans'}${data.length ? '. why not add some?' : ''}`);
 			return message.util.send({ embed });
 		}
 	}
