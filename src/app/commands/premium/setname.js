4e2eeb5109f64ad00ec2691e32c7fee785d7c57e
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-20 22:19:41 +0530

    Final Fix

--- src/app/commands/premium/setname.js
+++ src/app/commands/premium/setname.js
@@ -7,6 +7,7 @@ class SetNickNameCommand extends Command {
 			category: 'owner',
 			cooldown: 3000,
 			clientPermissions: ['EMBED_LINKS', 'MANAGE_NICKNAMES'],
+			userPermissions: ['MANAGE_GUILD'],
 			description: { content: 'Sets nickname of a member' }
 		});
 	}
