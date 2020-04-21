2faf368c02a1bd6793c8d8fa282e259bab766edc
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-04-21 21:00:09 +0530

    BugFix

--- src/app/commands/lookup/units.js
+++ src/app/commands/lookup/units.js
@@ -58,7 +58,7 @@ class UnitsCommand extends Command {
 	async exec(message, { data }) {
 		const embed = await this.embed(data, true);
 		const msg = await message.util.send({
-			embed: embed.setFooter('Level / Town-Hall & Builder-Hall Max')
+			embed: embed.setFooter('Level / Town Hall & Builder Hall Max')
 		});
 
 		for (const emoji of ['696655174025871461', '696292379703115780']) {
@@ -75,7 +75,7 @@ class UnitsCommand extends Command {
 			if (reaction.emoji.id === '696655174025871461') {
 				const embed = await this.embed(data, true);
 				await msg.edit({
-					embed: embed.setFooter('Level / Town-Hall & Builder-Hall Max')
+					embed: embed.setFooter('Level / Town Hall & Builder Hall Max')
 				});
 				await this.delay(250);
 				await reaction.users.remove(message.author.id);
@@ -102,7 +102,8 @@ class UnitsCommand extends Command {
 	async embed(data, option) {
 		const embed = new MessageEmbed()
 			.setColor(0x5970c1)
-			.setAuthor(`${data.name} (${data.tag})`, `https://coc.guide/static/imgs/other/town-hall-${data.townHallLevel}.png`, `https://link.clashofclans.com/?action=OpenPlayerProfile&tag=${data.tag.replace(/#/g, '')}`);
+			.setAuthor(`${data.name} (${data.tag})`, `https://coc.guide/static/imgs/other/town-hall-${data.townHallLevel}.png`, `https://link.clashofclans.com/?action=OpenPlayerProfile&tag=${data.tag.replace(/#/g, '')}`)
+			.setTitle(`Town Hall ${data.townHallLevel} ${data.builderHallLevel ? `| Builder Hall ${data.builderHallLevel}` : ''}`);
 
 		let index = 0;
 		let troopLevels = '';
@@ -122,8 +123,6 @@ class UnitsCommand extends Command {
 		});
 		if (troopLevels) {
 			embed.setDescription([
-				`**At Town-Hall ${data.townHallLevel} ${data.builderHallLevel ? `and Builder-Hall ${data.builderHallLevel}` : ''}**`,
-				'',
 				'**Elixir Troops**',
 				troopLevels.split('#').join('\n')
 			]);
