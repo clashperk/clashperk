9d16dac760703aa4b440675a67082d2ee1203692
Suvajit <suvajit.c@outlook.com>
2020-04-22 14:04:08 +0530

    Remove User Permission

--- src/app/commands/tracker/clangames.js
+++ src/app/commands/tracker/clangames.js
@@ -11,7 +11,7 @@ class ClanGamesCommand extends Command {
 			aliases: ['clangames', 'points', 'cg'],
 			category: 'tracker',
 			channel: 'guild',
-			userPermissions: ['MANAGE_GUILD'],
+			// userPermissions: ['MANAGE_GUILD'],
 			clientPermissions: ['ADD_REACTIONS', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
 			description: {
 				content: 'Shows clan game points of your clan members.',
