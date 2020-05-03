e5f520864c106a2a17c89868d33f9ce175871f30
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-03 20:17:42 +0530

    fix alias conflict

--- src/app/commands/activity/toggle.js
+++ src/app/commands/activity/toggle.js
@@ -5,7 +5,7 @@ const { ObjectId } = require('mongodb');
 class ToggleCommand extends Command {
 	constructor() {
 		super('toggle', {
-			aliases: ['toggle', 'toggle'],
+			aliases: ['toggle'],
 			category: 'activity',
 			channel: 'guild',
 			userPermissions: ['MANAGE_GUILD'],
