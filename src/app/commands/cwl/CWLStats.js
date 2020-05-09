c564f2b44988d7884ab41fe40a7d5f2c340c3217
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-09 19:42:49 +0530

    Fix

--- src/app/commands/cwl/CWLStats.js
+++ src/app/commands/cwl/CWLStats.js
@@ -96,10 +96,10 @@ class CWLStatsComamnd extends Command {
 							`**${clan.name}** vs **${opponent.name}**`,
 							`Ended ${moment.duration(Date.now() - end).format('D [days], H [hours] m [mins]', { trim: 'both mid' })} ago`
 						], [
-							`\`\`\`${data.clan.stars.toString().padEnd(19, ' ')} Stars ${data.opponent.stars.toString().padStart(18, ' ')}`,
+							`\`${data.clan.stars.toString().padEnd(19, ' ')} Stars ${data.opponent.stars.toString().padStart(18, ' ')}`,
 							`${data.clan.attacks.toString().padEnd(18, ' ')} Attacks ${data.opponent.attacks.toString().padStart(17, ' ')}`,
 							`${this.destruction(data.clan.destructionPercentage).padEnd(16, ' ')} Destruction ${this.destruction(data.opponent.destructionPercentage).padStart(15, ' ')}`,
-							'```'
+							'`'
 						]]);
 					}
 					if (data.state === 'inWar') {
@@ -108,10 +108,10 @@ class CWLStatsComamnd extends Command {
 							`**${clan.name}** vs **${opponent.name}**`,
 							`Started ${moment.duration(Date.now() - started).format('D [days], H [hours] m [mins]', { trim: 'both mid' })} ago`
 						], [
-							`\`\`\`${data.clan.stars.toString().padEnd(19, ' ')} Stars ${data.opponent.stars.toString().padStart(18, ' ')}`,
+							`\`${data.clan.stars.toString().padEnd(19, ' ')} Stars ${data.opponent.stars.toString().padStart(18, ' ')}`,
 							`${data.clan.attacks.toString().padEnd(18, ' ')} Attacks ${data.opponent.attacks.toString().padStart(17, ' ')}`,
 							`${this.destruction(data.clan.destructionPercentage).padEnd(16, ' ')} Destruction ${this.destruction(data.opponent.destructionPercentage).padStart(15, ' ')}`,
-							'```'
+							'`'
 						]]);
 					}
 					if (data.state === 'preparation') {
