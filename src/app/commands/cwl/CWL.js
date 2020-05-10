9aef12d8f198d8215b09a2ba7dbd599ba0bfaaf0
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-10 09:37:17 +0530

    Fix

--- src/app/commands/cwl/CWL.js
+++ src/app/commands/cwl/CWL.js
@@ -1,18 +1,63 @@
-const { Command } = require('discord-akairo');
+const { Command, Flag } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
 
 class CWLComamnd extends Command {
 	constructor() {
 		super('cwl', {
-			// aliases: ['cwl'],
-			category: 'owner',
-			cooldown: 3000,
+			aliases: ['cwl'],
+			category: 'cwl',
+			cooldown: 0,
 			description: {
-				content: 'You can\'t use it anyway, so why explain?'
+				content: '',
+				examples: [''],
+				usage: '<method> <...args>'
 			}
 		});
 	}
 
-	async exec(message) {}
+	*args(message) {
+		const prefix = this.handler.prefix(message);
+		const command = yield {
+			type: [
+				['cwl-attacks', 'attacks'],
+				['cwl-remaining', 'remaining'],
+				['cwl-round', 'round'],
+				['cwl-roster', 'roster'],
+				['cwl-stats', 'stats'],
+				['cwl-top', 'top'],
+				['cwl-members', 'members']
+			],
+			otherwise: new MessageEmbed()
+				.setAuthor('CWL Commands')
+				.setDescription([
+					'**Usage**',
+					`\`${prefix}cwl <method> <...args>\``,
+					'',
+					'**Available Methods**',
+					'',
+					'• roster `<clanTag>`',
+					'• round `<clanTag>`',
+					'• attacks `<clanTag>`',
+					'• remaining `<clanTag>`',
+					'• stats `<clanTag>`',
+					'• members `<clanTag>`',
+					'• top `<clanTag>`',
+					'',
+					'For additional `<...args>` usage refer to the examples below.',
+					'',
+					'**Examples**',
+					`\`${prefix}cwl roster #8QU8J9LP\``,
+					`\`${prefix}cwl round #8QU8J9LP\``,
+					`\`${prefix}cwl attacks #8QU8J9LP\``,
+					`\`${prefix}cwl remaining #8QU8J9LP\``,
+					`\`${prefix}cwl stats #8QU8J9LP\``,
+					`\`${prefix}cwl members #8QU8J9LP\``,
+					`\`${prefix}cwl top #8QU8J9LP\``
+				])
+		};
+
+		return Flag.continue(command);
+	}
 }
 
-// module.exports = CWLComamnd;
+module.exports = CWLComamnd;
