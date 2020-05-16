d70bfc95714ffd76ff71717d4e14c9a90fb5f12f
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-16 20:48:43 +0530

    Fix

--- src/app/core/ClanGamesEvent.js
+++ src/app/core/ClanGamesEvent.js
@@ -14,7 +14,7 @@ class ClanGames {
 		if (!this.event()) return this.flush();
 
 		// force update points
-		console.log(forced, tags);
+		console.log(forced, tags.map(t => t.tag));
 		const cache = this.cached.get(id);
 		if (cache && forced) {
 			const db = mongodb.db('clashperk').collection('clangames');
