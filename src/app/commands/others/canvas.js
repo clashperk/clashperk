ae0e7bde17a6c523989aae3a3a8c0f70e8d4e852
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-24 11:21:26 +0530

    canvas

---
+++ src/app/commands/others/canvas.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord-akairo');
+const { MessageAttachment } = require('discord.js');
+
+class CanvasCommand extends Command {
+	constructor() {
+		super('canvas', {
+			aliases: ['canvas', 'cnv'],
+			category: 'owner',
+			channel: 'guild',
+			description: {
+				content: 'You can\'t use this anyway, so why explain?'
+			},
+			args: [
+				{
+					id: 'txt',
+					match: 'content',
+					default: 'FUCK'
+				}
+			]
+		});
+	}
+
+	async exec(message, { txt }) {
+		const Canvas = require('canvas');
+		const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/707884431314124800/713991474404130816/airhounds.png');
+		const canvas = Canvas.createCanvas(1280, 720);
+		const ctx = canvas.getContext('2d');
+
+		ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+		ctx.font = '60px sans-seriff';
+		ctx.fillStyle = '#ffffff';
+		ctx.fillText(txt, canvas.width / 2.5, canvas.height / 1.8);
+
+		ctx.beginPath();
+		ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.clip();
+
+		const attachment = new MessageAttachment(canvas.toBuffer(), 'image.png');
+		return message.channel.send('', attachment);
+	}
+}
+
+module.exports = CanvasCommand;
