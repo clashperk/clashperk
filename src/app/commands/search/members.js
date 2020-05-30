7ee549db4df8bff213451c66056d40ff950836a4
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-30 14:13:38 +0530

    revert

--- src/app/commands/search/members.js
+++ src/app/commands/search/members.js
@@ -99,8 +99,7 @@ class MembersCommand extends Command {
 			embed: embed.setDescription([header, pages[0].join('\n')])
 				.setFooter('Page 1/2')
 		});
-
-		await msg.react('⬇️');
+		/* await msg.react('⬇️');
 		const collector = await msg.awaitReactions(
 			(reaction, user) => reaction.emoji.name === '⬇️' && user.id === message.author.id,
 			{ max: 1, time: 30000, errors: ['time'] }
@@ -110,7 +109,44 @@ class MembersCommand extends Command {
 		return message.channel.send({
 			embed: embed.setDescription([header, pages[1].join('\n')])
 				.setFooter('Page 2/2')
+		});*/
+
+		for (const emoji of ['⬅️', '➡️']) {
+			await msg.react(emoji);
+			await this.delay(250);
+		}
+
+		const collector = msg.createReactionCollector(
+			(reaction, user) => ['⬅️', '➡️'].includes(reaction.emoji.name) && user.id === message.author.id,
+			{ time: 45000, max: 10 }
+		);
+
+		collector.on('collect', async reaction => {
+			if (reaction.emoji.name === '➡️') {
+				await msg.edit({
+					embed: embed.setDescription([header, pages[1].join('\n')])
+						.setFooter('Page 2/2')
+				});
+				await this.delay(250);
+				await reaction.users.remove(message.author.id);
+				return message;
+			}
+			if (reaction.emoji.name === '⬅️') {
+				await msg.edit({
+					embed: embed.setDescription([header, pages[0].join('\n')])
+						.setFooter('Page 1/2')
+				});
+				await this.delay(250);
+				await reaction.users.remove(message.author.id);
+				return message;
+			}
 		});
+
+		collector.on('end', async () => {
+			await msg.reactions.removeAll().catch(() => null);
+			return message;
+		});
+		return message;
 	}
 
 	paginate(items, start, end) {
