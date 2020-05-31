ef22636932be22c528ecf4bc6b36ec8d1d08a219
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-05-31 10:49:59 +0530

    improve calculation

--- src/app/commands/search/currentwar.js
+++ src/app/commands/search/currentwar.js
@@ -130,11 +130,12 @@ class CurrentWarCommand extends Command {
 			{ level: 12, total: TH12 },
 			{ level: 13, total: TH13 }
 		].filter(townHall => townHall.total !== 0).reverse();
+		const Avg = townHalls.reduce((p, c) => p + (c.total * c.level), 0) / townHalls.reduce((p, c) => p + p.total, 0) || 0;
 		const math = (TH13 * 13) + (TH12 * 12) + (TH11 * 11) + (TH10 * 10) + (TH09 * 9) + (TH08 * 8) + (TH07 * 7) + (TH06 * 6) + (TH05 * 5) + (TH04 * 4) + (TH03 * 3) + (TH02 * 2) + Number(TH01);
 		const total = TH13 + TH12 + TH11 + TH10 + TH09 + TH08 + TH07 + TH06 + TH05 + TH04 + TH03 + TH02 + TH01;
 		const avg = math / total || 0;
 
-		return [`**(Avg: ${avg.toFixed(2)})**`, this.chunk(townHalls)
+		return [`**(Avg: ${Avg.toFixed(2)})**`, this.chunk(townHalls)
 			.map(chunks => chunks.map(th => `${townHallEmoji[th.level]} \`${th.total.toString().padStart(2, '0')}\``)
 				.join(' '))
 			.join('\n')].join('\n');
