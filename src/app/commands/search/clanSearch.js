84a6d089c2210d05f7e9b5bfeb207896a90c4c5c
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-03 23:09:50 +0530

    hyper link

--- src/app/commands/search/clanSearch.js
+++ src/app/commands/search/clanSearch.js
@@ -51,7 +51,7 @@ class ClanSearchCommand extends Command {
 						.replace(/closed/g, 'Closed')
 						.replace(/open/g, 'Open');
 					return [
-						`**${clan.name} (${clan.tag})**`,
+						`**[${clan.name} (${clan.tag})](https://www.clashofstats.com/clans/${clan.tag.substr(1)})**`,
 						`${clan.clanLevel} level, ${clan.members} members, ${clan.clanPoints} points`,
 						`${clanType}, ${clan.requiredTrophies} required ${clan.location ? `, ${clan.location.name}` : ''}`
 					].join('\n');
