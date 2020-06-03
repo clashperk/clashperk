4a7b1cb3dc23f03e662819c1b4a3e78c93836d16
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-03 23:15:19 +0530

    remove space

--- src/app/commands/search/clanSearch.js
+++ src/app/commands/search/clanSearch.js
@@ -53,7 +53,7 @@ class ClanSearchCommand extends Command {
 					return [
 						`**[${clan.name} (${clan.tag})](https://www.clashofstats.com/clans/${clan.tag.substr(1)})**`,
 						`${clan.clanLevel} level, ${clan.members} member${clan.members > 1 ? 's' : ''}, ${clan.clanPoints} points`,
-						`${clanType}, ${clan.requiredTrophies} required ${clan.location ? `, ${clan.location.name}` : ''}`
+						`${clanType}, ${clan.requiredTrophies} required${clan.location ? `, ${clan.location.name}` : ''}`
 					].join('\n');
 				}).join('\n\n')
 			]);
