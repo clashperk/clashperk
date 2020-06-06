ce6629c4491be19797e81b031ac385bbfdf415e9
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-06 20:35:07 +0530

    BugFix

--- src/app/commands/owner/reload.js
+++ src/app/commands/owner/reload.js
@@ -13,25 +13,21 @@ class ReloadCommand extends Command {
 	}
 
 	async exec(message) {
-		await this.client.shard.broadcastEval(
-			`[
-				this.inhibitorHandler.removeAll(),
-				this.listenerHandler.removeAll(),
-				this.commandHandler.removeAll(),
-				this.inhibitorHandler.loadAll(),
-				this.listenerHandler.loadAll(),
-				this.commandHandler.loadAll()
-			]`
-		).catch(() => null);
-		const cmd = this.client.commandHandler.modules.size;
-		const listener = this.client.listenerHandler.modules.size;
-		const inhibitor = this.client.inhibitorHandler.modules.size;
-		return message.util.send({
-			embed: {
-				color: 3093046,
-				description: `Reloaded ${cmd} commands, ${listener} listeners & ${inhibitor} inhibitors`
-			}
-		});
+		const inhibitor = this.client.inhibitorHandler.removeAll() && this.client.inhibitorHandler.loadAll();
+		const listener = this.client.listenerHandler.removeAll() && this.client.listenerHandler.loadAll();
+		const command = this.client.commandHandler.removeAll() && this.client.commandHandler.loadAll();
+
+		if (inhibitor && listener && command) {
+			const cmd = await this.client.commandHandler.modules.size;
+			const listener = await this.client.listenerHandler.modules.size;
+			const inhibitor = await this.client.inhibitorHandler.modules.size;
+			return message.util.send({
+				embed: {
+					color: 3093046,
+					description: `Reloaded ${cmd} commands, ${listener} listeners & ${inhibitor} inhibitors`
+				}
+			});
+		}
 	}
 }
 
--- src/app/commands/search/clan.js
+++ src/app/commands/search/clan.js
@@ -58,7 +58,7 @@ class ClanCommand extends Command {
 			.addField('Required Trophies', `${emoji.trophy} ${data.requiredTrophies}`, true)
 			.addField('Clan Type', clan_type, true)
 			.addField('Clan Points', `${emoji.trophy} ${data.clanPoints} ${emoji.versustrophy} ${data.clanVersusPoints}`, true)
-			.addField('War League', `${CWLEmoji[data.warLeague.name]} ${data.warLeague.name}`, true)
+			.addField('War League', `${CWLEmoji[data.warLeague.name] || ''} ${data.warLeague.name}`, true)
 			.addField('War Log', data.isWarLogPublic ? 'Public' : 'Private', true)
 			.addField('War Wins', data.warWins, true)
 			.addField('Win Streak', data.warWinStreak, true)
