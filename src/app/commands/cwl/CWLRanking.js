dffa886e377694b21488dd23946458ef19dea3ab
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-07 10:16:54 +0530

    CWL

---
+++ src/app/commands/cwl/CWLRanking.js
@@ -0,0 +1,257 @@
+const { Command, Argument, Flag } = require('discord-akairo');
+const fetch = require('node-fetch');
+const moment = require('moment');
+const { MessageEmbed } = require('discord.js');
+const { status } = require('../../util/constants');
+const Resolver = require('../../struct/Resolver');
+const { emoji } = require('../../util/emojis');
+
+class CWLRankingComamnd extends Command {
+	constructor() {
+		super('cwl-ranking', {
+			aliases: ['cwl-ranking'],
+			category: 'hidden',
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
+			description: {
+				content: 'Shows stats about current cwl war.',
+				usage: '<clanTag>',
+				examples: ['#8QU8J9LP']
+			}
+		});
+	}
+
+	cooldown(message) {
+		if (this.client.patron.isPatron(message.author, message.guild) || this.client.voteHandler.isVoter(message.author.id)) return 2000;
+		return 10000;
+	}
+
+	*args() {
+		const round = yield {
+			match: 'option',
+			flag: ['--round', '-r'],
+			type: Argument.range('integer', 1, 7, true)
+		};
+
+		const data = yield {
+			type: async (message, args) => {
+				const resolved = await Resolver.resolve(message, args);
+				if (resolved.status !== 200) {
+					await message.util.send({ embed: resolved.embed });
+					return Flag.cancel();
+				}
+				return resolved;
+			}
+		};
+
+		return { data, round };
+	}
+
+	async exec(message, { data }) {
+		await message.util.send(`**Fetching data... ${emoji.loading}**`);
+		const res = await fetch(`https://api.clashofclans.com/v1/clans/${encodeURIComponent(data.tag)}/currentwar/leaguegroup`, {
+			method: 'GET', timeout: 3000,
+			headers: { accept: 'application/json', authorization: `Bearer ${process.env.DEVELOPER_TOKEN}` }
+		}).catch(() => null);
+
+		if (!res) {
+			return message.util.send({
+				embed: {
+					color: 0xf30c11,
+					author: { name: 'Error' },
+					description: status(504)
+				}
+			});
+		}
+
+		const body = await res.json();
+
+		const embed = this.client.util.embed()
+			.setColor(0x5970c1);
+
+		if (!(body.state || res.ok)) {
+			embed.setAuthor(`${data.name} (${data.tag})`, data.badgeUrls.medium, `https://link.clashofclans.com/?action=OpenClanProfile&tag=${data.tag}`)
+				.setThumbnail(data.badgeUrls.medium)
+				.setDescription('Clan is not in CWL');
+			return message.util.send({ embed });
+		}
+
+		return this.rounds(message, body, { clanTag: data.tag, clanName: data.name, clanBadge: data.badgeUrls.medium });
+	}
+
+	async rounds(message, body, { clanTag, clanName, clanBadge } = {}) {
+		const collection = [];
+		const rounds = body.rounds.filter(r => !r.warTags.includes('#0'));
+		let [index, stars, destruction] = [0, 0, 0];
+		const ranking = body.clans.map(clan => ({ tag: clan.tag, stars: 0 }));
+		const members = body.clans.find(clan => clan.tag === clanTag)
+			.members.map(member => ({ name: member.name, tag: member.tag, stars: 0, attacks: 0, of: 0 }));
+
+		for (const { warTags } of rounds) {
+			for (const warTag of warTags) {
+				const res = await fetch(`https://api.clashofclans.com/v1/clanwarleagues/wars/${encodeURIComponent(warTag)}`, {
+					method: 'GET', headers: { accept: 'application/json', authorization: `Bearer ${process.env.DEVELOPER_TOKEN}` }
+				});
+				const data = await res.json();
+				this.ranking(data, ranking);
+
+				if ((data.clan && data.clan.tag === clanTag) || (data.opponent && data.opponent.tag === clanTag)) {
+					const clan = data.clan.tag === clanTag ? data.clan : data.opponent;
+					const opponent = data.clan.tag === clanTag ? data.opponent : data.clan;
+					if (data.state === 'warEnded') {
+						stars += this.winner(clan, opponent) ? clan.stars + 10 : clan.stars;
+						destruction += clan.destructionPercentage * data.teamSize;
+						const end = new Date(moment(data.endTime).toDate()).getTime();
+						for (const member of clan.members) {
+							members.find(m => m.tag === member.tag)
+								.of += 1;
+							if (member.attacks) {
+								members.find(m => m.tag === member.tag)
+									.attacks += 1;
+
+								members.find(m => m.tag === member.tag)
+									.stars += member.attacks[0].stars;
+							}
+						}
+
+						collection.push([[
+							`${this.isWinner(clan, opponent)} **${clan.name}** vs **${opponent.name}**`,
+							`${emoji.clock_small} [Round ${++index}] Ended ${moment.duration(Date.now() - end).format('D[d], H[h] m[m]', { trim: 'both mid' })} ago`
+						], [
+							`\`${clan.stars.toString().padEnd(14, ' ')} Stars ${opponent.stars.toString().padStart(14, ' ')}\``,
+							`\`${this.attacks(clan.attacks, data.teamSize).padEnd(13, ' ')} Attacks ${this.attacks(opponent.attacks, data.teamSize).padStart(13, ' ')}\``,
+							`\`${this.destruction(clan.destructionPercentage).padEnd(11, ' ')} Destruction ${this.destruction(opponent.destructionPercentage).padStart(11, ' ')}\``
+						]]);
+					}
+					if (data.state === 'inWar') {
+						stars += clan.stars;
+						destruction += clan.destructionPercentage * data.teamSize;
+						const started = new Date(moment(data.startTime).toDate()).getTime();
+						for (const member of clan.members) {
+							members.find(m => m.tag === member.tag)
+								.of += 1;
+							if (member.attacks) {
+								members.find(m => m.tag === member.tag)
+									.attacks += 1;
+
+								members.find(m => m.tag === member.tag)
+									.stars += member.attacks[0].stars;
+							}
+						}
+
+						collection.push([[
+							`${emoji.loading} **${clan.name}** vs **${opponent.name}**`,
+							`${emoji.clock_small} [Round ${++index}] Started ${moment.duration(Date.now() - started).format('D[d], H[h] m[m]', { trim: 'both mid' })} ago`
+						], [
+							`\`${clan.stars.toString().padEnd(14, ' ')} Stars ${opponent.stars.toString().padStart(14, ' ')}\``,
+							`\`${this.attacks(clan.attacks, data.teamSize).padEnd(13, ' ')} Attacks ${this.attacks(opponent.attacks, data.teamSize).padStart(13, ' ')}\``,
+							`\`${this.destruction(clan.destructionPercentage).padEnd(11, ' ')} Destruction ${this.destruction(opponent.destructionPercentage).padStart(11, ' ')}\``
+						]]);
+					}
+				}
+			}
+		}
+
+		/* if (!collection.length) {
+			return message.util.send({
+				embed: {
+					description: 'CWL stats are available after round 2'
+				}
+			});
+		}*/
+		const description = collection.map(arr => {
+			const header = arr[0].join('\n');
+			const description = arr[1].join('\n');
+			return [header, description].join('\n');
+		}).join('\n\n');
+		const rank = ranking.sort((a, b) => b.stars - a.stars).findIndex(a => a.tag === clanTag);
+		const leaderboard = members.sort((a, b) => b.stars - a.stars);
+		const embed = new MessageEmbed()
+			.setColor(0x5970c1)
+			.setAuthor(`${clanName} CWL`, clanBadge)
+			.setDescription(description)
+			.setFooter(`Rank ${rank + 1}, ${stars} Stars, ${destruction.toFixed()}% Destruction`);
+		const msg = await message.util.send({ embed });
+		await msg.react('➕');
+		const collector = await msg.awaitReactions(
+			(reaction, user) => reaction.emoji.name === '➕' && user.id === message.author.id,
+			{ max: 1, time: 30000, errors: ['time'] }
+		).catch(() => null);
+		if (!msg.deleted) await msg.reactions.removeAll().catch(() => null);
+		if (!collector || !collector.size) return;
+		return message.channel.send({
+			embed: {
+				color: 0x5970c1,
+				author: {
+					name: `${clanName} CWL`,
+					icon_url: clanBadge
+				},
+				description: [
+					`\`\u200e # STAR ATTACK  ${'NAME'.padEnd(20, ' ')}\``,
+					leaderboard.filter(m => m.attacks !== 0)
+						.map((m, i) => `\`\u200e${(++i).toString().padStart(2, ' ')} ${m.stars.toString().padStart(4, ' ')} ${this.attacks(m.attacks, m.of).padStart(6, ' ')}  ${m.name.padEnd(20, ' ')}\``).join('\n')
+				].join('\n')
+			}
+		});
+	}
+
+	destruction(dest) {
+		return dest.toFixed(2).toString().concat('%');
+	}
+
+	attacks(num, team) {
+		return num.toString().concat(`/${team}`);
+	}
+
+	isWinner(clan, opponent) {
+		if (clan.stars > opponent.stars) {
+			return emoji.ok;
+		} else if (clan.stars < opponent.stars) {
+			return emoji.wrong;
+		}
+		if (clan.destructionPercentage > opponent.destructionPercentage) {
+			return emoji.ok;
+		} else if (clan.destructionPercentage < opponent.destructionPercentage) {
+			return emoji.wrong;
+		}
+		return emoji.empty;
+	}
+
+	winner(clan, opponent) {
+		if (clan.stars > opponent.stars) {
+			return true;
+		} else if (clan.stars < opponent.stars) {
+			return false;
+		}
+		if (clan.destructionPercentage > opponent.destructionPercentage) {
+			return true;
+		} else if (clan.destructionPercentage < opponent.destructionPercentage) {
+			return false;
+		}
+	}
+
+	ranking(data, ranking) {
+		if (data.state === 'warEnded') {
+			ranking.find(({ tag }) => tag === data.clan.tag)
+				.stars += this.winner(data.clan, data.opponent)
+					? data.clan.stars + 10
+					: data.clan.stars;
+
+			ranking.find(({ tag }) => tag === data.opponent.tag)
+				.stars += this.winner(data.opponent, data.clan)
+					? data.opponent.stars + 10
+					: data.opponent.stars;
+		}
+
+		if (data.state === 'inWar') {
+			ranking.find(({ tag }) => tag === data.clan.tag)
+				.stars += data.clan.stars;
+
+			ranking.find(({ tag }) => tag === data.opponent.tag)
+				.stars += data.opponent.stars;
+		}
+
+		return ranking;
+	}
+}
+
+module.exports = CWLRankingComamnd;
