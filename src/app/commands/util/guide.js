3856a5ec4a9bc8081a5a71563cb1e684d0760b89
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-10 11:49:07 +0530

    no link

--- src/app/commands/util/guide.js
+++ src/app/commands/util/guide.js
@@ -8,7 +8,7 @@ class GuideCommand extends Command {
 			category: 'util',
 			cooldown: 1000,
 			clientPermissions: ['EMBED_LINKS'],
-			description: { content: 'Shows [info](https://clashperk.xyz/) about how to use bot.' }
+			description: { content: 'Shows info about how to use bot.' }
 		});
 	}
 
