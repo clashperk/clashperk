1226fcd52fa39ebf06815e1b566b7c054974c091
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-06-15 13:58:35 +0530

    test

---
+++ src/app/commands/search/cping.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord-akairo');
+
+class ThCompoCommand extends Command {
+	constructor() {
+		super('cping', {
+			aliases: ['cping'],
+			category: 'util',
+			description: {
+				content: 'Pings Clash of Clans API',
+				usage: '<clanTag>',
+				examples: ['#2Q98URCGY', '2Q98URCGY']
+			}
+		});
+	}
+
+	async exec(message) {
+		const hrStart = process.hrtime();
+		try {
+			await this.client.coc.locations({ limit: 1 });
+		} catch (e) {
+			console.log(e.toString());
+			return message.util.send('**Clash API Ping Failed!**');
+		}
+
+		const diff = process.hrtime(hrStart);
+		const sec = diff[0] > 0 ? `${diff[0].toFixed(2)} sec` : null;
+		return message.util.send(`**CLash API Ping ${sec || `${(diff[1] / 1000000).toFixed(2)} ms`}**`);
+	}
+}
+
+module.exports = ThCompoCommand;
