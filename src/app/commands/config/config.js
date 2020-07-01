f0664a39bfba904dcf40545b881d2f6ac385ba70
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-01 21:09:09 +0530

    update config command

--- src/app/commands/config/config.js
+++ src/app/commands/config/config.js
@@ -22,7 +22,7 @@ class ConfigCommand extends Command {
 			.setAuthor(`Settings of ${message.guild.name}`)
 			.addField('Prefix', this.handler.prefix(message))
 			.addField('Patron', this.client.patron.get(message.guild.id, 'guild', false) ? `Active ${emoji.authorize}` : 'None')
-			.addField('Color', `#${color.toString(16)}` || `#${0x5970c1.toString(16)} (default)`);
+			.addField('Color', color ? `#${color.toString(16)}` : null || `#${0x5970c1.toString(16)} (default)`);
 
 		return message.util.send({ embed });
 	}
