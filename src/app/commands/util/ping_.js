9a0d9f455b51b70ceeb41e7abf63d45e0ba891e0
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-09 21:50:53 +0530

    update patron

---
+++ src/app/commands/util/ping_.js
@@ -0,0 +1,56 @@
+const { Command, Flag } = require('discord-akairo');
+const { mongodb } = require('../../struct/Database');
+
+class PingCommand extends Command {
+	constructor() {
+		super('pin_g', {
+			aliases: ['pin_g', 'pog'],
+			category: 'hidden',
+			ownerOnly: true,
+			description: {
+				content: 'Pings me!'
+			}
+		});
+	}
+
+	cooldown(message) {
+		if (this.client.patron.check(message.author, message.guild)) return 1000;
+		return 3000;
+	}
+
+	async exec(message) {
+		await mongodb.db('clashperk').collection('clanstores')
+			.updateMany({}, {
+				$set: {
+					active: true,
+					patron: false
+				}
+			})
+			.then(console.log());
+
+		await mongodb.db('clashperk').collection('clanstores')
+			.updateMany({ frozen: true }, {
+				$set: {
+					active: false,
+					patron: false
+				},
+				$unset: { frozen: '' }
+			})
+			.then(console.log());
+
+		for (const guild of this.client.guilds.cache.values()) {
+			if (!this.client.patron.get(guild.id, 'guild', false)) continue;
+
+			await mongodb.db('clashperk').collection('clanstores')
+				.updateMany({ guild: guild.id }, {
+					$set: {
+						active: true,
+						patron: true
+					}
+				})
+				.then(console.log());
+		}
+	}
+}
+
+module.exports = PingCommand;
