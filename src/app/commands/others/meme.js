b8ffa85a9173f9476c5ded7ed0ac3c6e790e513c
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-10 21:44:57 +0530

    catch { }

--- src/app/commands/others/meme.js
+++ src/app/commands/others/meme.js
@@ -20,19 +20,21 @@ class MemeCommand extends Command {
 
 	async exec(message) {
 		const page = Math.floor(Math.random() * 100) + 1;
-		const res = await fetch(`https://api.imgur.com/3/gallery/r/memes/all/${page}`, {
-			method: 'GET',
-			headers: { Authorization: `Client-ID ${process.env.IMGUR}` }
-		});
-		const data = await res.json();
+		try {
+			const res = await fetch(`https://api.imgur.com/3/gallery/r/memes/all/${page}`, {
+				method: 'GET',
+				headers: { Authorization: `Client-ID ${process.env.IMGUR}` }
+			});
+			const data = await res.json();
 
-		const image = Math.floor(Math.random() * data.data.length) + 1;
-		const embed = new MessageEmbed()
-			.setColor(this.client.embed(message))
-			.setTitle(data.data[image].title.toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase()))
-			.setURL(data.data[image].link)
-			.setImage(data.data[image].link);
-		return message.channel.send({ embed });
+			const image = Math.floor(Math.random() * data.data.length) + 1;
+			const embed = new MessageEmbed()
+				.setColor(this.client.embed(message))
+				.setTitle(data.data[image].title.toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase()))
+				.setURL(data.data[image].link)
+				.setImage(data.data[image].link);
+			return message.channel.send({ embed });
+		} catch { }
 	}
 }
 
