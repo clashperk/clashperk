bb0710f07488236c26d3aa11d2d1193fbe9c9a7f
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-11 19:10:38 +0530

    Update Logger

--- src/app/util/logger.js
+++ src/app/util/logger.js
@@ -8,36 +8,36 @@ class Logger {
 	}
 
 	debug(message, { color = 'yellow', tag = '[DEBUG]', label } = {}) {
-		this._write(message, { color, tag, label });
+		this.constructor.write(message, { color, tag, label });
 	}
 
-	info(message, { color = 'cyan', tag = '[INFO ]', label } = {}) {
-		this._write(message, { color, tag, label });
+	info(message, { color = 'cyan', tag = '[INFO]', label } = {}) {
+		this.constructor.write(message, { color, tag, label });
 	}
 
 	error(message, { color = 'red', tag = '[ERROR]', label } = {}) {
-		this._write(message, { color, tag, label, error: true });
+		this.constructor.write(message, { color, tag, label, error: true });
 	}
 
-	warn(message, { color = 'magenta', tag = '[WARN ]', label } = {}) {
-		this._write(message, { color, tag, label });
+	warn(message, { color = 'magenta', tag = '[WARN]', label } = {}) {
+		this.constructor.write(message, { color, tag, label });
 	}
 
-	_write(message, { color, tag, label = 'UNKNOWN', error = false } = {}) {
+	static write(message, { color, tag, label = 'UNKNOWN', error = false } = {}) {
 		const timestamp = chalk.cyan(moment().format('DD-MM-YYYY kk:mm:ss'));
-		const content = this._clean(message);
+		const content = this.clean(message);
 		const stream = error ? process.stderr : process.stdout;
-		const shard = this._shard(this.client);
+		const shard = this.shard(this.client);
 		stream.write(`[${timestamp}]${shard} ${chalk[color].bold(tag)} » [${label}] » ${content}\n`);
 	}
 
-	_clean(item) {
+	static clean(item) {
 		if (typeof item === 'string') return item;
 		const cleaned = inspect(item, { depth: Infinity });
 		return cleaned;
 	}
 
-	_shard(client) {
+	static shard(client) {
 		return client && client.shard && client.shard.ids ? ` [SHARD ${client.shard.ids[0]}]` : '';
 	}
 }
