cfa696e36251b43e69c4623cdaa4bb6ed6e9d841
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-11 12:58:33 +0530

    enough

--- src/app/commands/cwl/CWLmissed.js
+++ src/app/commands/cwl/CWLmissed.js
@@ -104,8 +104,12 @@ class CWLMissedComamnd extends Command {
 		}
 
 		const collection = Object.values(object);
-		if (!collection.length) {
+		if (rounds.length < 3 && !collection.length) {
 			return message.util.send('This command is available after the end of a round.');
+		}
+
+		if (!collection.length) {
+			return message.util.send('Hmm! Looks like everyone attacked.');
 		}
 
 		const clan = body.clans.find(clan => clan.tag === clanTag);
@@ -113,10 +117,7 @@ class CWLMissedComamnd extends Command {
 			.setColor(this.client.embed(message))
 			.setAuthor(`${clan.name} (${clan.tag})`, clan.badgeUrls.small)
 			.setTitle('Missed Attacks')
-			.setDescription([
-				...collection.sort((a, b) => b.count - a.count)
-					.map(m => `${redNum[m.count]} ${m.name}`)
-			])
+			.setDescription(collection.sort((a, b) => b.count - a.count).map(m => `${redNum[m.count]} ${m.name}`))
 			.setFooter(`Upto Round #${round}`);
 
 		return message.util.send({ embed });
