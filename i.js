9793571dc7928bceefa93f704edbc674e6f69337
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-07-20 19:00:01 +0530

    delete i.js

--- i.js
+++
@@ -1,7 +0,0 @@
-const name = ['0'];
-const txt = 'AH |';
-
-if (txt.length && txt.trim().startsWith('|')) name.push(txt);
-else if (txt.length && txt.trim().endsWith('|')) name.unshift(txt);
-
-console.log(name);
