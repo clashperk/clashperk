e817f44458a547f56f0833cc75d97f98c36d8260
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-08-05 23:32:20 +0530

    update about command

--- src/app/commands/util/about.js
+++ src/app/commands/util/about.js
@@ -1,5 +1,4 @@
 const { Command } = require('discord-akairo');
-const { oneLine } = require('common-tags');
 const { emoji } = require('../../util/emojis');
 
 class AboutCommand extends Command {
@@ -21,19 +20,19 @@ class AboutCommand extends Command {
 		const owner = await this.client.users.fetch(this.client.ownerID, false);
 		const embed = this.client.util.embed()
 			.setColor(this.client.embed(message))
-			.setAuthor(`${this.client.user.username}`, this.client.user.displayAvatarURL(), 'https://clashperk.xyz')
+			.setAuthor(`${this.client.user.username}`, this.client.user.displayAvatarURL(), 'https://clashperk.com')
 			.setDescription([
 				'Feature-Rich and Powerful Clash of Clans Discord bot with everything you will ever need.'
 			])
 			.addField('Owner', `${emoji.botdev} **${owner.tag}**`)
 			.addField('Library', `${emoji.nodejs} [discord.js](https://discord.js.org)`)
-			.addField('Website', '[https://clashperk.com](https://clashperk.com \'Feature-Rich and Powerful Clash of Clans Discord bot with everything you will ever need.\')')
-			.addField('Need help?', 'Join [Official Discord](https://discord.gg/ppuppun)')
-			.addField('Do you like the bot?', 'Please support on us [Patreon](https://www.patreon.com/clashperk)')
+			.addField('Website', '[https://clashperk.com](https://clashperk.com/)')
+			.addField('Need help?', 'Join [Support Discord](https://discord.gg/ppuppun)')
+			.addField('Do you like the bot?', '[Become a Patron](https://www.patreon.com/clashperk)')
 			.addField('Legal Notice', [
-				oneLine(`This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it.
-				For more information see Supercell\'s [Fan Content Policy](https://www.supercell.com/fan-content-policy 'Fan Content Policy × Supercell')`)
-			]);
+				'This content is not affiliated with, endorsed, sponsored, or specifically approved by Supercell and Supercell is not responsible for it.',
+				'For more information see Supercell\'s [Fan Content Policy](https://www.supercell.com/fan-content-policy \'Fan Content Policy × Supercell\')'
+			].join(' '));
 		return message.util.send({ embed });
 	}
 }
