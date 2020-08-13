7b95ddb3d0c88f20d331f7c2f6adbff6ae9e231f
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-08-13 20:30:28 +0530

    Revert

--- src/app/commands/util/ping.js
+++ src/app/commands/util/ping.js
@@ -17,28 +17,23 @@ class PingCommand extends Command {
 	}
 
 	async exec(message) {
-		const permissions = ['ADD_REACTIONS', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'];
+		// const permissions = ['ADD_REACTIONS', 'EMBED_LINKS', 'USE_EXTERNAL_EMOJIS', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'];
 		const msg = await message.util.send('Pinging~');
 		const latency = (msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp);
 		return message.util.send([
 			`**Gateway Ping~ ${latency.toString()}ms**`,
-			`**API Ping~ ${Math.round(this.client.ws.ping).toString()}ms**`,
-			!message.channel.permissionsFor(message.guild.me).has(permissions, false)
-				? `**Missing Permission~ ${this.missingPermissions(message.channel, this.client.user, permissions)}**`
-				: ''
+			`**API Ping~ ${Math.round(this.client.ws.ping).toString()}ms**`
 		]);
 	}
 
 	missingPermissions(channel, user, permissions) {
 		const missingPerms = channel.permissionsFor(user).missing(permissions)
 			.map(str => {
-				if (str === 'VIEW_CHANNEL') return '`Read Messages`';
-				return `\`${str.replace(/_/g, ' ').toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase())}\``;
+				if (str === 'VIEW_CHANNEL') return 'Read Messages';
+				return str.replace(/_/g, ' ').toLowerCase().replace(/\b(\w)/g, char => char.toUpperCase());
 			});
 
-		return missingPerms.length > 1
-			? `${missingPerms.slice(0, -1).join(', ')} and ${missingPerms.slice(-1)[0]}`
-			: missingPerms[0];
+		return missingPerms.join('\n');
 	}
 }
 
