d5b04ce87c37d301feec39697684506208b52545
Suvajit <suvajit.me@gmail.com>
2020-08-30 12:21:56 +0530

    Update clanwarlog.js

--- src/app/commands/activity/clanwarlog.js
+++ src/app/commands/activity/clanwarlog.js
@@ -81,7 +81,7 @@ class WarLogCommand extends Command {
 			patron: this.client.patron.get(message.guild.id, 'guild', false)
 		});
 
-		this.client.cacheHandler.add(id, {
+		this.client.cacheHandler.add({
 			op: Op.CLAN_WAR_LOG,
 			guild: message.guild.id,
 			tag: data.tag
