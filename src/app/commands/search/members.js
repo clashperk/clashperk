bcd189dd059d247bcddffea3a833d18ede43de5b
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-09-08 09:32:28 +0530

    bug fixes

--- src/app/commands/search/members.js
+++ src/app/commands/search/members.js
@@ -87,9 +87,9 @@ class MembersCommand extends Command {
 		const header = stripIndent(`${emoji.trophy} **\`\u200eTH ${'TAG'.padEnd(10, ' ')} ${'NAME'.padEnd(15, '\u2002')}\`**`);
 		const pages = [
 			this.paginate(townhall ? filter : items, 0, 25)
-				.items.map(member => `${leagueEmoji[member.league]} \`\u200e${this.padStart(member.townHallLevel)} ${member.tag.padEnd(10, '\u2002')} ${Util.escapeInlineCode(member.name.substring(0, 15)).padEnd(15, '\u2002')}\u200b .\u200f\``),
+				.items.map(member => `${leagueEmoji[member.league]} \`\u200e${this.padStart(member.townHallLevel)} ${member.tag.padEnd(10, '\u2002')} ${member.name.substring(0, 15).replace(/\`/g, '\\').padEnd(15, '\u2002')}\u200b .\u200f\``),
 			this.paginate(townhall ? filter : items, 25, 50)
-				.items.map(member => `${leagueEmoji[member.league]} \`\u200e${this.padStart(member.townHallLevel)} ${member.tag.padEnd(10, '\u2002')} ${Util.escapeInlineCode(member.name.substring(0, 15)).padEnd(15, '\u2002')}\u200b. \u200f\``)
+				.items.map(member => `${leagueEmoji[member.league]} \`\u200e${this.padStart(member.townHallLevel)} ${member.tag.padEnd(10, '\u2002')} ${member.name.substring(0, 15).padEnd(15, '\u2002')}\u200b. \u200f\``)
 		];
 
 		if (!pages[1].length) return message.util.send({ embed: embed.setDescription([header, pages[0].join('\n')]) });
