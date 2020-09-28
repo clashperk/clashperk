4b130f13c6949c7c699aaa3be5247a14c54a72b1
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-09-28 22:44:40 +0530

    ability to set no-color

--- src/app/commands/config/color.js
+++ src/app/commands/config/color.js
@@ -1,4 +1,4 @@
-const { Command } = require('discord-akairo');
+const { Command, Argument } = require('discord-akairo');
 
 class ColorCommand extends Command {
 	constructor() {
@@ -17,12 +17,12 @@ class ColorCommand extends Command {
 					'[Become a Patron](https://www.patreon.com/join/clashperk)'
 				],
 				usage: '<color>',
-				examples: ['#0080ff']
+				examples: ['#0080ff', 'none']
 			},
 			args: [
 				{
 					id: 'hexColor',
-					type: 'color',
+					type: Argument.union(['none'], 'color'),
 					default: message => this.client.embed(message)
 				}
 			]
@@ -34,16 +34,14 @@ class ColorCommand extends Command {
 		return 3000;
 	}
 
-	async exec(message, { hexColor }) {
+	async exec(message, { color }) {
 		if (!this.client.patron.check(message.author, message.guild)) {
 			return this.handler.handleDirectCommand(message, 'color', this.handler.modules.get('help'), false);
 		}
-		this.client.settings.set(message.guild, 'color', hexColor);
+
+		this.client.settings.set(message.guild, 'color', color);
 		return message.util.send({
-			embed: {
-				description: 'Display color updated.',
-				color: hexColor
-			}
+			embed: { description: 'Display color updated.', color }
 		});
 	}
 }
