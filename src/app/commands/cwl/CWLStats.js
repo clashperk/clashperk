48b50eb322e5b3239aa2dd921d10f70e633acf30
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-10-07 12:15:41 +0530

    12

--- src/app/commands/cwl/CWLStats.js
+++ src/app/commands/cwl/CWLStats.js
@@ -163,7 +163,7 @@ class CWLStatsComamnd extends Command {
 							`${this.winner(clan, opponent) ? emoji.ok : emoji.wrong} **${clan.name}** vs **${opponent.name}**`,
 							`${emoji.clock_small} [Round ${++index}] Ended ${moment.duration(Date.now() - end).format('D[d], H[h] m[m]', { trim: 'both mid' })} ago`
 						], [
-							`\`${clan.stars.toString().padEnd(14, ' ')} Stars ${opponent.stars.toString().padStart(14, ' ')}\``,
+							`\`${clan.stars.toString().padEnd(12, ' ')} Stars ${opponent.stars.toString().padStart(12, ' ')}\``,
 							`\`${this.attacks(clan.attacks, data.teamSize).padEnd(11, ' ')} Attacks ${this.attacks(opponent.attacks, data.teamSize).padStart(11, ' ')}\``,
 							`\`${this.destruction(clan.destructionPercentage).padEnd(9, ' ')} Destruction ${this.destruction(opponent.destructionPercentage).padStart(9, ' ')}\``
 						]]);
@@ -200,7 +200,7 @@ class CWLStatsComamnd extends Command {
 							`${emoji.loading} **${clan.name}** vs **${opponent.name}**`,
 							`${emoji.clock_small} [Round ${++index}] Started ${moment.duration(Date.now() - started).format('D[d], H[h] m[m]', { trim: 'both mid' })} ago`
 						], [
-							`\`${clan.stars.toString().padEnd(14, ' ')} Stars ${opponent.stars.toString().padStart(14, ' ')}\``,
+							`\`${clan.stars.toString().padEnd(12, ' ')} Stars ${opponent.stars.toString().padStart(12, ' ')}\``,
 							`\`${this.attacks(clan.attacks, data.teamSize).padEnd(11, ' ')} Attacks ${this.attacks(opponent.attacks, data.teamSize).padStart(11, ' ')}\``,
 							`\`${this.destruction(clan.destructionPercentage).padEnd(9, ' ')} Destruction ${this.destruction(opponent.destructionPercentage).padStart(9, ' ')}\``
 						]]);
