df4b86fc5c1c5e1b4e5cc7e7e1f3025052385926
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-10-12 21:40:23 +0530

    test command

---
+++ src/app/commands/search/player2.js
@@ -0,0 +1,141 @@
+const { Command, Flag } = require('discord-akairo');
+const { MessageEmbed, Util } = require('discord.js');
+const moment = require('moment');
+const { mongodb } = require('../../struct/Database');
+const Resolver = require('../../struct/Resolver');
+const { leagueId } = require('../../util/constants');
+const { emoji, townHallEmoji, heroEmoji, leagueEmoji, starEmoji } = require('../../util/emojis');
+
+const roles = {
+	member: 'Member',
+	admin: 'Elder',
+	coLeader: 'Co-Leader',
+	leader: 'Leader'
+};
+
+class PlayerCommand extends Command {
+	constructor() {
+		super('player2', {
+			aliases: ['player2', 'p2'],
+			category: 'search2',
+			clientPermissions: ['EMBED_LINKS', 'USE_EXTERNAL_EMOJIS'],
+			description: {
+				content: 'Shows info about your in-game profile.',
+				usage: '<playerTag>',
+				examples: ['#9Q92C8R20']
+			}
+		});
+	}
+
+	*args() {
+		const data = yield {
+			type: async (message, args) => {
+				const resolved = await Resolver.resolve(message, args, true);
+				if (resolved.status !== 200) {
+					await message.channel.send({ embed: resolved.embed });
+					return Flag.cancel();
+				}
+				return resolved;
+			}
+		};
+
+		return { data };
+	}
+
+	cooldown(message) {
+		if (this.client.patron.check(message.author, message.guild)) return 1000;
+		return 3000;
+	}
+
+	async exec(message, { data }) {
+		const embed = new MessageEmbed()
+			.setColor(this.client.embed(message))
+			.setTitle(`${Util.escapeMarkdown(data.name)} (${data.tag})`)
+			.setURL(`https://link.clashofclans.com/?action=OpenPlayerProfile&tag=${encodeURIComponent(data.tag)}`)
+			.setDescription([
+				`${townHallEmoji[data.townHallLevel]} **${data.townHallLevel}** ${leagueEmoji[data.league ? data.league.id : 29000000]} **${data.trophies}** ${emoji.warstar} **${data.warStars}**`,
+				'',
+				'**Achievement Stats**',
+				`${emoji.donated} Troops Donated: 312`,
+				`${emoji.spelldonation} Spells Donated: 12`,
+				'<:cg:765244426444079115> Clan Games Points: 4,875'
+			]);
+
+		embed.addField('Town Hall & XP', [
+			`${townHallEmoji[data.townHallLevel]} ${data.townHallLevel}`,
+			`${emoji.xp} ${data.expLevel}`
+		], true);
+		embed.addField('Current League', [
+			`${emoji.trophy} ${data.trophies}`,
+			`${leagueEmoji[data.league ? data.league.id : 29000000]} ${data.league ? data.league.name : 'Unranked'}`
+		], true);
+
+		embed.addField('Best Trophies', `${leagueEmoji[leagueId(data.bestTrophies)]} **${data.bestTrophies}**`, true);
+
+		embed.addField('War Stars', `${emoji.warstar} ${data.warStars}`, true);
+		embed.addField('Attacks/Defenses', `${emoji.attacksword} ${data.attackWins} ${emoji.shield} ${data.defenseWins}`, true);
+
+		embed.addField('Donations/Receives', [
+			`${emoji.troopsdonation} ${data.donations}${emoji.donated} ${data.donationsReceived}${emoji.received}`
+		], true);
+
+		data.achievements.forEach(achievement => {
+			if (achievement.name === 'Friend in Need') {
+				embed.addField('Friend in Need', `${starEmoji[achievement.stars]} ${achievement.value}`, true);
+			}
+			if (achievement.name === 'Games Champion') {
+				embed.addField('Clan Games Points', `${starEmoji[achievement.stars]} ${achievement.value}`, true);
+			}
+			if (achievement.name === 'War League Legend') {
+				embed.addField('CWL Stars', `${starEmoji[achievement.stars]} ${achievement.value}`, true);
+			}
+		});
+
+		if (data.clan) {
+			embed.addField(`Clan ${roles[data.role]}`, `${emoji.clan} [${data.clan.name}](https://link.clashofclans.com/?action=OpenClanProfile&tag=${encodeURIComponent(data.clan.tag)})`);
+		}
+
+		let heroLevels = '';
+		data.heroes.forEach(hero => {
+			if (hero.village === 'home') {
+				if (hero.level === hero.maxLevel) {
+					heroLevels += `${heroEmoji[hero.name]} **${hero.level}**\u2002\u2002`;
+				} else {
+					heroLevels += `${heroEmoji[hero.name]} ${hero.level}\u2002\u2002`;
+				}
+			}
+		});
+		if (heroLevels) embed.addField('Heroes', heroLevels);
+
+
+		const flag = await this.flag(message, data.tag);
+		if (flag) {
+			const user = await this.client.users.fetch(flag.user, false).catch(() => null);
+			const offset = await this.offset(message);
+			embed.addField('Flag', [
+				`${flag.reason}`,
+				`\`${user ? user.tag : 'Unknown#0000'} (${moment(flag.createdAt).utcOffset(offset).format('DD-MM-YYYY kk:mm')})\``
+			]);
+		}
+
+		return message.util.send({ embed });
+	}
+
+	async flag(message, tag) {
+		const data = await mongodb.db('clashperk')
+			.collection('flaggedusers')
+			.findOne({ guild: message.guild.id, tag });
+		return data;
+	}
+
+	async offset(message) {
+		const data = await mongodb.db('clashperk').collection('timezoneoffset').findOne({ user: message.author.id });
+		const prefix = data?.timezone?.offset < 0 ? '-' : '+';
+		const seconds = Math.abs(data?.timezone?.offset ?? 0);
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor(seconds % 3600 / 60);
+		return `${prefix}${hours >= 1 ? `0${hours}`.slice(-2) : '00'}:${minutes >= 1 ? `0${minutes}`.slice(-2) : '00'}`;
+	}
+}
+
+module.exports = PlayerCommand;
