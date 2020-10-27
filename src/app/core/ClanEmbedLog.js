2aa0bb7c2ed5aaa21613f0877bedcfe38dd8c3ec
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-10-27 22:23:56 +0530

    last try

--- src/app/core/ClanEmbedLog.js
+++ src/app/core/ClanEmbedLog.js
@@ -29,7 +29,7 @@ class ClanEmbed {
 	}
 
 	async throttle(id) {
-		if (this.lastReq.count >= 4) await this.delay(2000);
+		if (this.lastReq.id === id) await this.delay(2000);
 
 		if (this.lastReq.id === id) {
 			this.lastReq.count += 1;
