8db4fde9b043ed5e453fdd20c195394b3f3b2183
SUVAJIT CHHATUI <suvajit.me@gmail.com>
2020-11-04 11:21:15 +0530

    change 404 status message

--- src/app/util/constants.js
+++ src/app/util/constants.js
@@ -2,7 +2,7 @@ const codes = {
 	504: '504 Request Timeout',
 	400: 'Client provided incorrect parameters for the request.',
 	403: 'Access denied, either because of missing/incorrect credentials or used API token does not grant access to the requested resource.',
-	404: 'Resource was not found.',
+	404: 'Looks like the tag is invalid!',
 	429: 'Request was throttled, because amount of requests was above the threshold defined for the used API token.',
 	500: 'Unknown error happened when handling the request.',
 	503: 'Service is temporarily unavailable because of maintenance.'
