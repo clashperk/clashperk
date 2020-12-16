4bd48b4013b66944d94d30dcc27f3cd1af0f4599
SUVAJIT <suvajit.me@gmail.com>
2020-12-16 10:26:42 +0530

    Fix Typo

--- src/app/commands/cwl/CWLExport.js
+++ src/app/commands/cwl/CWLExport.js
@@ -159,7 +159,7 @@ class CWLExport extends Command {
 								member.attacks += 1;
 								member.stars += m.attacks[0].stars;
 								member.dest += m.attacks[0].destructionPercentage;
-								member.starTypes.push(member.stars);
+								member.starTypes.push(m.stars);
 							}
 
 							if (m.bestOpponentAttack) {
