39f3e7c00a737137893bfe7b156a43a44d02876b
SUVAJIT <suvajit.me@gmail.com>
2020-12-31 13:24:00 +0530

    commandBlocked listener fixed

--- src/bot/listeners/commandHandler/CommandBlocked.ts
+++ src/bot/listeners/commandHandler/CommandBlocked.ts
@@ -2,7 +2,7 @@ import { Listener, Command } from 'discord-akairo';
 import { TextChannel, Message } from 'discord.js';
 
 interface Text {
-	[key: string]: string;
+	[key: string]: string | null;
 }
 
 const texts: Text = {
@@ -25,7 +25,8 @@ export default class CommandBlockedListener extends Listener {
 		const label = message.guild ? `${message.guild.name}/${message.author.tag}` : `${message.author.tag}`;
 		this.client.logger.debug(`${command.id} ~ ${reason}`, { label });
 
-		if (msg && message.guild ? (message.channel as TextChannel).permissionsFor(this.client.user!)?.has('SEND_MESSAGES') : true) {
+		if (!msg) return;
+		if (message.guild ? (message.channel as TextChannel).permissionsFor(this.client.user!)?.has('SEND_MESSAGES') : true) {
 			return message.channel.send(msg);
 		}
 	}
