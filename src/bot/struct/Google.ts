da75b8960cb56f40a3813ef5ce6a0d5375e07279
Suvajit <suvajit.me@gmail.com>
2021-02-14 21:01:01 +0530

    Node 14.x consistency

--- src/bot/struct/Google.ts
+++ src/bot/struct/Google.ts
@@ -1,27 +1,23 @@
 import fetch from 'node-fetch';
 import qs from 'querystring';
 
+const GOOGLE_MAPS_API_BASE_URL = 'https://maps.googleapis.com/maps/api';
+
 export default {
 	async location(query: any) {
 		const search = qs.stringify({
 			address: query,
 			key: process.env.GOOGLE
 		});
-		const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?${search}`)
-			.catch(() => null);
 
-		if (!res?.ok) return null;
-		const data = await res.json();
-		if (data.status !== 'OK') return null;
-
-		return data;
+		return fetch(`${GOOGLE_MAPS_API_BASE_URL}/geocode/json?${search}`)
+			.then(res => res.json()).catch(() => null);
 	},
 
 	async timezone(query: any) {
-		const raw = await this.location(query);
-		if (!raw) return null;
-		const location = raw.results[0];
+		const location = (await this.location(query))?.results[0];
 		if (!location) return null;
+
 		const search = qs.stringify({
 			key: process.env.GOOGLE,
 			timestamp: new Date().getTime() / 1000
@@ -30,13 +26,10 @@ export default {
 		const lat = location.geometry.location.lat as string;
 		const lng = location.geometry.location.lng as string;
 
-		const res = await fetch(
-			`https://maps.googleapis.com/maps/api/timezone/json?${search}&location=${lat},${lng}`
-		).catch(() => null);
+		const timezone = await fetch(`${GOOGLE_MAPS_API_BASE_URL}/timezone/json?${search}&location=${lat},${lng}`)
+			.then(res => res.json()).catch(() => null);
 
-		if (!res?.ok) return null;
-		const data = await res.json();
-
-		return { location: raw, timezone: data };
+		if (!timezone) return null;
+		return { location, timezone };
 	}
 };
