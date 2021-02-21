3d983f0c1d81e199f0f689228fb6c6712a4aca14
Suvajit <suvajit.me@gmail.com>
2021-02-21 23:08:26 +0530

    Fix unlink channel command

--- src/bot/commands/link/Unlink.ts
+++ src/bot/commands/link/Unlink.ts
@@ -1,6 +1,6 @@
-import { Command, PrefixSupplier } from 'discord-akairo';
-import { Message, MessageEmbed } from 'discord.js';
-import { COLLECTIONS } from '../../util/Constants';
+import { Command, Argument, PrefixSupplier } from 'discord-akairo';
+import { Message, TextChannel, MessageEmbed } from 'discord.js';
+import { Collections } from '@clashperk/node';
 
 export default class UnlinkCommand extends Command {
 	public constructor() {
@@ -27,17 +27,17 @@ export default class UnlinkCommand extends Command {
 	}
 
 	public *args(msg: Message) {
-		const tag = yield {
+		const parsed = yield {
 			flag: '--tag',
-			type: (msg: Message, tag: string) => this.parseTag(tag),
-			match: msg.hasOwnProperty('token') ? 'option' : 'phrase'
+			match: msg.hasOwnProperty('token') ? 'option' : 'phrase',
+			type: Argument.union('textChannel', (msg, tag) => this.parseTag(tag))
 		};
 
-		return { tag };
+		return { parsed };
 	}
 
-	public async exec(message: Message, { tag }: { tag?: string }) {
-		if (!tag) {
+	public async exec(message: Message, { parsed, parsed: tag }: { parsed?: TextChannel | string }) {
+		if (!parsed) {
 			const prefix = (this.handler.prefix as PrefixSupplier)(message) as string;
 			const embed = new MessageEmbed()
 				.setColor(this.client.embed(message))
@@ -56,9 +56,28 @@ export default class UnlinkCommand extends Command {
 			);
 		}
 
-		const deleted = await this.delete(message.author.id, tag);
+		if (parsed && parsed instanceof TextChannel) {
+			if (!message.member!.permissions.has('MANAGE_GUILD')) {
+				return message.util!.send('You are missing `Manage Server` permission to use this comamnd.');
+			}
+
+			const { value } = await this.client.storage.collection.findOneAndUpdate(
+				{ channels: parsed.id }, { $pull: { channels: parsed.id } }, { returnOriginal: false }
+			);
+
+			if (value) {
+				return message.util!.send(
+					`Successfully deleted **${value.name} (${value.tag})** from <#${parsed.id}>`
+				);
+			}
+
+			// eslint-disable-next-line @typescript-eslint/no-base-to-string
+			return message.util!.send(`Couldn\'t find any clan linked to ${parsed.toString()}`);
+		}
+
+		const deleted = await this.delete(message.author.id, tag as string);
 		if (!deleted) {
-			const clan = await this.client.db.collection(COLLECTIONS.LINKED_CLANS)
+			const clan = await this.client.db.collection(Collections.LINKED_CLANS)
 				.findOneAndDelete({ user: message.author.id, tag });
 			if (clan.value?.tag) return message.util!.send({ embed: { description: `Successfully deleted **${clan.value.tag as string}**` } });
 
@@ -74,7 +93,7 @@ export default class UnlinkCommand extends Command {
 
 	private async delete(id: string, tag: string) {
 		const dLink = await this.client.http.unlinkPlayerTag(tag);
-		const data = await this.client.db.collection(COLLECTIONS.LINKED_USERS)
+		const data = await this.client.db.collection(Collections.LINKED_PLAYERS)
 			.findOneAndUpdate({ user: id }, { $pull: { entries: { tag } } });
 		return data.value?.entries?.includes(tag) ? { tag } : dLink ? { tag } : null;
 	}
