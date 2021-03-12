a12a11114d965646f5e905ac3095d41e1fe22d9f
Suvajit <suvajit.me@gmail.com>
2021-03-12 11:01:23 +0530

    add some example with image

--- src/bot/commands/link/Alias.ts
+++ src/bot/commands/link/Alias.ts
@@ -1,4 +1,5 @@
 import { Command, Flag } from 'discord-akairo';
+import { Message } from 'discord.js';
 
 export default class AliasCommand extends Command {
 	public constructor() {
@@ -25,7 +26,11 @@ export default class AliasCommand extends Command {
 					'add AH #8QU8J9LP',
 					'remove AH',
 					'list'
-				]
+				],
+				image: {
+					url: 'https://i.imgur.com/8fWqtY5.png',
+					text: ''
+				}
 			}
 		});
 	}
@@ -36,7 +41,8 @@ export default class AliasCommand extends Command {
 				['alias-list', 'list'],
 				['alias-add', 'add', 'create'],
 				['alias-remove', 'remove', 'delete']
-			]
+			],
+			otherwise: (msg: Message) => this.handler.handleDirectCommand(msg, 'alias', this.handler.modules.get('help')!)
 		};
 
 		return Flag.continue(sub);
