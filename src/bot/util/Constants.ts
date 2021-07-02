f28801ed6d66ec6dbd9acde339cb899b743ba19d
SUVAJIT <suvajit.me@gmail.com>
2021-07-02 16:53:30 +0530

    delete duplicate typings

--- src/bot/util/Constants.ts
+++ src/bot/util/Constants.ts
@@ -206,27 +206,6 @@ export interface TroopInfo {
 	maxLevel: number;
 }
 
-export interface UserInfo {
-	user_id: string; // unique: true
-	user_tag?: string;
-	clan?: {
-		tag: string; // indexed
-		name?: string;
-	};
-	entries: {
-		tag: string; // unique: true, sparse: true
-		name?: string;
-		verified: boolean; // indexed
-		unknown: boolean;
-		townhall?: number;
-	}[];
-	timezone?: {
-		id: string; // indexed
-		name: string; // indexed
-		offset: number;
-	};
-}
-
 export const TROOPS_HOUSING = [
 	{
 		hall: 1,
