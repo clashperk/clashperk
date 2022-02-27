b4c4a3a606770bb4089a4a0346514fe118578cf9
Suvajit <suvajit.me@gmail.com>
2022-02-27 20:45:32 +0530

    delete prefix command

--- src/bot/commands/config/Config.ts
+++ src/bot/commands/config/Config.ts
@@ -1,5 +1,6 @@
-import { Message } from 'discord.js';
-import { Command, PrefixSupplier, Flag } from 'discord-akairo';
+import { Message, TextChannel } from 'discord.js';
+import { Argument, Command, PrefixSupplier } from 'discord-akairo';
+import { Settings } from '../../util/Constants';
 
 export default class ConfigCommand extends Command {
 	public constructor() {
@@ -9,50 +10,67 @@ export default class ConfigCommand extends Command {
 			clientPermissions: ['EMBED_LINKS'],
 			channel: 'guild',
 			description: {
-				content: ['Manage or view server settings.'],
-				examples: ['color #f96854', 'prefix ?'],
-				usage: ['[prefix|color] <...args>']
-			}
+				content: ['Manage server configuration.'],
+				examples: [],
+				usage: []
+			},
+			optionFlags: ['--color_code', '--events_channel']
 		});
 	}
 
 	public *args(): unknown {
-		const sub = yield {
-			type: [
-				['config-color', 'color'],
-				['config-prefix', 'prefix'],
-				['config-events', 'events']
-			],
-			otherwise: (msg: Message) => this.handler.runCommand(msg, this, {})
+		const channel = yield {
+			match: 'option',
+			flag: '--events_channel',
+			type: Argument.union(['none', 'reset'], 'textChannel')
 		};
 
-		return Flag.continue(sub);
+		const color = yield {
+			'match': 'option',
+			'flag': '--color_code',
+			'type': Argument.union(['none', 'reset'], 'color'),
+			'default': (message: Message) => this.client.embed(message)
+		};
+
+		return { color, channel };
 	}
 
-	public exec(message: Message) {
-		const color = this.client.settings.get<number>(message.guild!, 'color', undefined);
+	public exec(message: Message, { color, channel }: { color?: number | string; channel?: TextChannel | string }) {
+		if (color) {
+			if (['reset', 'none'].includes(color as string)) {
+				this.client.settings.delete(message.guild!, 'color');
+				color = this.client.embed(message);
+			}
+			this.client.settings.set(message.guild!, 'color', color);
+		}
+
+		if (channel) {
+			if (['reset', 'none'].includes(channel as string)) {
+				this.client.settings.delete(message.guild!, Settings.EVENTS_CHANNEL);
+			} else if (channel instanceof TextChannel) {
+				this.client.settings.set(message.guild!, Settings.EVENTS_CHANNEL, channel.id);
+			}
+		}
+
+		return this.fallback(message);
+	}
+
+	public async fallback(message: Message) {
+		const color = this.client.settings.get<number>(message.guild!, Settings.COLOR, null);
 		const prefix = (this.handler.prefix as PrefixSupplier)(message) as string;
+
+		const channelId = this.client.settings.get<string>(message.guild!, Settings.EVENTS_CHANNEL, null);
+		const channel = message.guild!.channels.cache.get(channelId);
+
 		const embed = this.client.util.embed()
 			.setColor(this.client.embed(message))
 			.setAuthor({ name: `Settings of ${message.guild!.name}` })
 			.addField('Prefix', prefix)
 			.addField('Patron', this.client.patrons.get(message.guild!.id) ? 'Yes' : 'No')
-			.addField('Color', color ? `#${color.toString(16).toUpperCase()}` : 'None');
+			.addField('Color', color ? `#${color.toString(16).toUpperCase()}` : 'None')
+			// eslint-disable-next-line @typescript-eslint/no-base-to-string
+			.addField('Events Channel', channel ? channel.toString() : 'None');
 
-		const embeds = [
-			embed,
-			this.client.util.embed()
-				.setColor(this.client.embed(message))
-				.setDescription([
-					`\`${prefix}config ${this.description.usage as string}\``,
-					'',
-					this.description.content.join('\n'),
-					'',
-					'**Examples**',
-					this.description.examples.map((en: string) => `\`${prefix}config ${en}\``).join('\n')
-				].join('\n'))
-		];
-
-		return message.util!.send({ embeds });
+		return message.util!.send({ embeds: [embed] });
 	}
 }
