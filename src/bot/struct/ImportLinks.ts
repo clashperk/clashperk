7b55386dc435dd7e2e55ffb4ac0091976686a934
suvajit <suvajit.me@gmail.com>
2023-01-15 01:16:59 +0530

    update username

--- src/bot/struct/ImportLinks.ts
+++ src/bot/struct/ImportLinks.ts
@@ -8,18 +8,18 @@ export class ImportLinks {
 	}
 
 	public async init() {
-		const cursor = this.client.db.collection(Collections.PLAYER_LINKS).find({ name: null });
+		const cursor = this.client.db.collection(Collections.PLAYER_LINKS).find({ username: null });
 		let count = 0;
 		while (await cursor.hasNext()) {
 			count++;
 			const data = await cursor.next();
 			if (!data) continue;
-			const player = await this.client.http.player(data.tag);
-			if (!player.ok) continue;
+			const user = await this.client.users.fetch(data.userId).catch(() => null);
+			if (!user) continue;
 
 			await this.client.db
 				.collection(Collections.PLAYER_LINKS)
-				.updateOne({ _id: new ObjectId(data._id) }, { $set: { name: player.name } });
+				.updateOne({ _id: new ObjectId(data._id) }, { $set: { username: user.tag } });
 			console.log(`[${count}] Updated ${data.tag as string} accounts for ${data.username as string}`);
 		}
 		console.log('Done');
