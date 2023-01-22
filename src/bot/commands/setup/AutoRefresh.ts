fb236ef9c808ab948af98185642e7ee61ee5935a
suvajit <suvajit.me@gmail.com>
2023-01-22 21:08:06 +0530

    bug fix with /autorole refresh

--- src/bot/commands/setup/AutoRefresh.ts
+++ src/bot/commands/setup/AutoRefresh.ts
@@ -1,7 +1,6 @@
 import { CommandInteraction } from 'discord.js';
 import { Command } from '../../lib/index.js';
-import { PlayerLinks } from '../../types/index.js';
-import { Collections, Settings } from '../../util/Constants.js';
+import { Settings } from '../../util/Constants.js';
 import { Util } from '../../util/index.js';
 
 export default class AutoTownHallRoleCommand extends Command {
@@ -11,8 +10,8 @@ export default class AutoTownHallRoleCommand extends Command {
 			channel: 'guild',
 			userPermissions: ['ManageGuild'],
 			clientPermissions: ['EmbedLinks', 'ManageRoles'],
-			defer: true,
-			ephemeral: true
+			defer: true
+			// ephemeral: true
 		});
 	}
 
@@ -30,43 +29,49 @@ export default class AutoTownHallRoleCommand extends Command {
 		this.client.settings.set(interaction.guildId, Settings.ROLE_REFRESHED, Date.now());
 		await interaction.editReply('Started refreshing roles...');
 
-		const collection = this.client.db.collection<PlayerLinks>(Collections.PLAYER_LINKS);
+		// const collection = this.client.db.collection<PlayerLinks>(Collections.PLAYER_LINKS);
 		for (const clan of clans) {
 			const data = await this.client.http.clan(clan.tag);
 			if (!data.ok) continue;
 
-			const links = await collection.find({ tag: { $in: data.memberList.map((mem) => mem.tag) } }).toArray();
-			const unknowns = await this.client.http.getDiscordLinks(data.memberList);
+			// const links = await collection.find({ tag: { $in: data.memberList.map((mem) => mem.tag) } }).toArray();
+			// const unknowns = await this.client.http.getDiscordLinks(data.memberList);
 
-			for (const { userId, tag } of unknowns) {
-				if (links.find((mem) => mem.tag === tag && mem.userId === userId)) continue;
-				const lastAccount = await collection.findOne({ userId }, { sort: { order: -1 } });
+			// for (const { userId, tag } of unknowns) {
+			// 	if (links.find((mem) => mem.tag === tag && mem.userId === userId)) continue;
+			// 	const lastAccount = await collection.findOne({ userId }, { sort: { order: -1 } });
 
-				const player = data.memberList.find((mem) => mem.tag === tag) ?? (await this.client.http.player(tag));
-				if (!player.name) continue;
+			// 	const player = data.memberList.find((mem) => mem.tag === tag) ?? (await this.client.http.player(tag));
+			// 	if (!player.name) continue;
 
-				const user = await this.client.users.fetch(userId).catch(() => null);
-				if (!user) continue;
+			// 	const user = await this.client.users.fetch(userId).catch(() => null);
+			// 	if (!user) continue;
 
-				try {
-					await collection.insertOne({
-						userId: user.id,
-						username: user.tag,
-						tag,
-						name: player.name,
-						verified: false,
-						order: lastAccount?.order ? lastAccount.order + 1 : 0,
-						createdAt: new Date()
-					});
-				} catch {}
-			}
+			// 	try {
+			// 		await collection.insertOne({
+			// 			userId: user.id,
+			// 			username: user.tag,
+			// 			tag,
+			// 			name: player.name,
+			// 			verified: false,
+			// 			order: lastAccount?.order ? lastAccount.order + 1 : 0,
+			// 			createdAt: new Date()
+			// 		});
+			// 	} catch {}
+			// }
 
+			await interaction.editReply(`Refreshing clan roles for ${clan.name}...`);
 			await this.client.rpcHandler.roleManager.queue(data, {});
-			await Util.delay(1000 * 30);
+			await Util.delay(1000 * 5);
+
+			await interaction.editReply(`Refreshing town hall roles for ${clan.name}...`);
 			await this.client.rpcHandler.roleManager.queue(data, { isThRole: true });
-			await Util.delay(1000 * 30);
+			await Util.delay(1000 * 5);
+
+			await interaction.editReply(`Refreshing league roles for ${clan.name}...`);
 			await this.client.rpcHandler.roleManager.queue(data, { isLeagueRole: true });
+			await Util.delay(1000 * 5);
 		}
-		return interaction.editReply('Successfully refreshed roles.');
+		return interaction.editReply('Successfully refreshed roles.').catch(() => null);
 	}
 }
