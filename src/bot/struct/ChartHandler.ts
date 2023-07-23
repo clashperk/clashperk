60b664d7025e09f452268ef635592ee44c990983
suvajit <suvajit.me@gmail.com>
2023-07-24 00:39:41 +0530

    fix charts for usage graphs

--- src/bot/struct/ChartHandler.ts
+++ src/bot/struct/ChartHandler.ts
@@ -1,75 +1,8 @@
 import fetch from 'node-fetch';
-import { nanoid } from 'nanoid';
 
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const sizes = {
-	1: { width: 500, height: 300 },
-	3: { width: 800, height: 400 },
-	7: { width: 800, height: 400 },
-	24: { width: 800, height: 400 }
-};
-
 export default {
-	async activity(collection: any[] = [], title: string[], days = 1): Promise<string> {
-		const colors = ['#266ef7', '#c63304', '#ffc107', '#50c878', '#ffac75', '#4dc1fa', '#cb5aff'];
-		days = collection.length > 3 ? 3 : days;
-		const datasets = collection.map((obj: any, i) => ({
-			label: obj.name,
-			type: days > 7 ? 'bar' : 'line',
-			fill: false,
-			backgroundColor: colors[i],
-			borderColor: colors[i],
-			borderWidth: 2,
-			pointBorderColor: colors[i],
-			pointBackgroundColor: colors[i],
-			pointRadius: 2,
-			data: [...obj.data.map((d: any) => d.count)]
-		}));
-
-		const body = {
-			backgroundColor: 'white',
-			width: sizes[days as keyof typeof sizes].width,
-			height: sizes[days as keyof typeof sizes].height,
-			devicePixelRatio: 2.0,
-			format: 'png',
-			chart: {
-				type: 'bar',
-				data: {
-					labels: [...collection[0].data.map((d: any) => d.short)],
-					datasets: [...datasets]
-				},
-				options: {
-					responsive: false,
-					legend: {
-						position: 'top',
-						display: true,
-						labels: {
-							fontSize: 10,
-							padding: 4
-						}
-					},
-					title: {
-						display: true,
-						fontSize: 10,
-						padding: 2,
-						text: [...title]
-					}
-				}
-			}
-		};
-
-		const url = `https://chart.clashperk.com/${nanoid()}`;
-		await fetch(url, {
-			body: JSON.stringify(body),
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		}).then((res) => res.json());
-		return url;
-	},
-
 	async growth(collection: any[] = [], { addition, deletion, growth }: { addition: number; deletion: number; growth: number }) {
 		const total = collection.reduce((pre, curr) => Number(pre) + Number(curr.value.addition - curr.value.deletion), 0);
 		const body = {
@@ -127,15 +60,13 @@ export default {
 			}
 		};
 
-		const url = `https://chart.clashperk.com/${nanoid()}`;
-		await fetch(url, {
+		const data = (await fetch(`https://quickchart.io/chart/create`, {
 			body: JSON.stringify(body),
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			}
-		}).then((res) => res.json());
-
-		return url;
+		}).then((res) => res.json())) as { url: string };
+		return data.url;
 	}
 };
