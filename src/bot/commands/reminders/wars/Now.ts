dfcad59ff7bc70462f41e1c2fb24e8f819690e9f
suvajit <suvajit.me@gmail.com>
2024-03-05 21:08:41 +0530

    exclude prep day from instant reminder

--- src/bot/commands/reminders/wars/Now.ts
+++ src/bot/commands/reminders/wars/Now.ts
@@ -249,8 +249,7 @@ export default class ReminderNowCommand extends Command {
 		for (const tag of reminder.clans) {
 			const currentWars = await this.client.http.getCurrentWars(tag);
 			for (const data of currentWars) {
-				// if (!data.ok) continue;
-				if (['notInWar', 'warEnded'].includes(data.state)) continue;
+				if (['notInWar', 'warEnded', 'preparation'].includes(data.state)) continue;
 
 				const warType = data.warTag ? 'cwl' : data.isFriendly ? 'friendly' : 'normal';
 				if (!reminder.warTypes.includes(warType)) continue;
