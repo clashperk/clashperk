46d6558ee390027260bb517fd2ea38dc2c6421ee
Xenon <79185767+Xenon27@users.noreply.github.com>
2024-10-04 17:58:28 +0200

    cmd: /attacks clan:

--- scripts/commands.ts
+++ scripts/commands.ts
@@ -502,7 +502,7 @@ export const COMMANDS: RESTPostAPIApplicationCommandsJSONBody[] = [
     description_localizations: translation('command.attacks.description'),
     options: [
       {
-        name: 'tag',
+        name: 'clan',
         description: common.options.tag.description,
         description_localizations: translation('common.options.tag.description'),
         type: ApplicationCommandOptionType.String,
