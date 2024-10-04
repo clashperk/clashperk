c003e6bb20ab08639bebdbc716cd2b052ba26a06
Xenon <79185767+Xenon27@users.noreply.github.com>
2024-10-04 18:10:31 +0200

    cmd: /warlog clan:

--- scripts/commands.ts
+++ scripts/commands.ts
@@ -1028,7 +1028,7 @@ export const COMMANDS: RESTPostAPIApplicationCommandsJSONBody[] = [
     description_localizations: translation('command.warlog.description'),
     options: [
       {
-        name: 'tag',
+        name: 'clan',
         description: common.options.tag.description,
         description_localizations: translation('common.options.tag.description'),
         autocomplete: true,
