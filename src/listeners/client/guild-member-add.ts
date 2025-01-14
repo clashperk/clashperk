12eed913385c892ef774190dd3c61918b09bd09e
suvajit <suvajit.me@gmail.com>
2025-01-14 21:19:57 +0530

    fix: use 3 sec delay before adding roles

--- src/listeners/client/guild-member-add.ts
+++ src/listeners/client/guild-member-add.ts
@@ -1,6 +1,7 @@
 import { Settings } from '@app/constants';
 import { GuildMember } from 'discord.js';
 import { Listener } from '../../lib/handlers.js';
+import { Util } from '../../util/toolkit.js';
 
 export default class GuildMemberAddListener extends Listener {
   public constructor() {
@@ -15,8 +16,9 @@ export default class GuildMemberAddListener extends Listener {
     if (this.client.settings.hasCustomBot(member.guild) && !this.client.isCustom()) return;
 
     if (this.client.settings.get(member.guild, Settings.USE_AUTO_ROLE, true)) {
+      await Util.delay(3000);
       const autoRoleAllowNotLinked = this.client.settings.get<boolean>(member.guild, Settings.AUTO_ROLE_ALLOW_NOT_LINKED, true);
-      return this.client.rolesManager.updateOne(member.user, member.guild.id, true, autoRoleAllowNotLinked);
+      await this.client.rolesManager.updateOne(member.user, member.guild.id, true, autoRoleAllowNotLinked);
     }
   }
 }
