574d06fa33e7805700065cc7c2f9ef4d055f4199
suvajit <suvajit.me@gmail.com>
2025-01-25 23:31:03 +0530

    fix: disable message capture

--- src/listeners/client/message-capture.ts
+++ src/listeners/client/message-capture.ts
@@ -1,6 +1,6 @@
 import { Message } from 'discord.js';
 import { Listener } from '../../lib/handlers.js';
-import { mixpanel } from '../../struct/mixpanel.js';
+// import { mixpanel } from '../../struct/mixpanel.js';
 
 export default class MessageCaptureListener extends Listener {
   public constructor() {
@@ -15,19 +15,19 @@ export default class MessageCaptureListener extends Listener {
     if (!message.applicationId) return;
     if (message.applicationId !== this.client.user?.id) return;
 
-    mixpanel.track('Message Posted', {
-      distinct_id: this.client.user!.id,
-      guild_id: message.guild ? message.guild.id : 'DM',
-      guild_name: message.guild ? message.guild.name : 'DM',
-      user_id: message.author.id,
-      display_name: message.author.displayName
-    });
+    // mixpanel.track('Message Posted', {
+    //   distinct_id: this.client.user!.id,
+    //   guild_id: message.guild ? message.guild.id : 'DM',
+    //   guild_name: message.guild ? message.guild.name : 'DM',
+    //   user_id: message.author.id,
+    //   display_name: message.author.displayName
+    // });
 
-    mixpanel.people.set(this.client.user!.id, {
-      $first_name: message.author.displayName,
-      username: message.author.username,
-      user_id: this.client.user!.id,
-      locale: 'en-US'
-    });
+    // mixpanel.people.set(this.client.user!.id, {
+    //   $first_name: message.author.displayName,
+    //   username: message.author.username,
+    //   user_id: this.client.user!.id,
+    //   locale: 'en-US'
+    // });
   }
 }
