02207a14fd21d648d9996b5fe577d49cd39ea5ba
Suvajit <suvajit.me@gmail.com>
2025-10-02 12:58:27 +0530

    Bump version from 3.10.63 to 3.10.64 (#282)
    * Bump version from 3.10.63 to 3.10.64
    
    * Update season ID logic in leaderboard command
    
    * Change season identifier from prevId to oldId

--- src/commands/legend/legend-leaderboard.ts
+++ src/commands/legend/legend-leaderboard.ts
@@ -41,7 +41,7 @@ export default class LegendLeaderboardCommand extends Command {
     const { clans, resolvedArgs } = await this.client.storage.handleSearch(interaction, { args: args.clans });
     if (!clans) return;
 
-    let seasonId = args.season ?? Season.ID;
+    let seasonId = args.season ?? Season.oldId;
 
     const isDefaultMessage = interaction.isMessageComponent() && interaction.message.type === MessageType.Default;
     if (isDefaultMessage) {
@@ -86,7 +86,7 @@ export default class LegendLeaderboardCommand extends Command {
       sort_by: args.sort_by,
       limit: args.limit,
       is_bb: args.is_bb,
-      season: args.season && args.season !== Season.ID ? args.season : null,
+      season: args.season && args.season !== Season.oldId ? args.season : null,
       export_disabled: args.export_disabled
     };
 
@@ -146,7 +146,7 @@ export default class LegendLeaderboardCommand extends Command {
         )
     );
 
-    if (seasonId !== Season.ID && !args.season) {
+    if (seasonId !== Season.oldId && !args.season) {
       return interaction.editReply({ embeds: [embed], components: [] });
     }
 
