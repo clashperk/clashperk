b729ff3cde6746d3c7441b098bb7dc68e1317513
suvajit <suvajit.me@gmail.com>
2026-01-12 20:29:41 +0530

    fix: /units command

--- src/commands/search/units.ts
+++ src/commands/search/units.ts
@@ -18,7 +18,7 @@ import {
   SUPER_TROOPS,
   TOWN_HALLS
 } from '../../util/emojis.js';
-import { getMenuFromMessage, unitsFlatten } from '../../util/helper.js';
+import { getMenuFromMessage, padEnd, padStart, unitsFlatten } from '../../util/helper.js';
 import { RAW_SUPER_TROOPS, RAW_TROOPS_WITH_ICONS, TroopJSON } from '../../util/troops.js';
 
 export default class UnitsCommand extends Command {
@@ -42,12 +42,12 @@ export default class UnitsCommand extends Command {
 
   public async exec(
     interaction: CommandInteraction | ButtonInteraction<'cached'>,
-    args: { tag?: string; user?: User; max_level?: boolean }
+    args: { tag?: string; user?: User; max_level?: boolean; equipment_only?: boolean }
   ) {
     const data = await this.client.resolver.resolvePlayer(interaction, args.tag ?? args.user?.id);
     if (!data) return;
 
-    const embed = this.embed(data, Boolean(args.max_level))
+    const embed = this.embed(data, Boolean(args.max_level), Boolean(args.equipment_only))
       .setColor(this.client.embed(interaction))
       .setDescription(
         `Units for TH${data.townHallLevel} Max ${data.builderHallLevel ? `and BH${data.builderHallLevel} Max` : ''}`
@@ -55,24 +55,25 @@ export default class UnitsCommand extends Command {
     if (!interaction.isMessageComponent()) await interaction.editReply({ embeds: [embed] });
     if (!interaction.inCachedGuild()) return;
 
-    const payload = {
-      cmd: this.id,
-      tag: data.tag
-    };
-
+    const payload = { cmd: this.id, tag: data.tag };
     const customIds = {
       accounts: JSON.stringify({ ...payload, string_key: 'tag' }),
       refresh: JSON.stringify({ ...payload }),
       player: JSON.stringify({ ...payload, cmd: 'player' }),
       upgrades: JSON.stringify({ ...payload, cmd: 'upgrades' }),
       rushed: JSON.stringify({ ...payload, cmd: 'rushed' }),
-      maxLevel: JSON.stringify({ ...payload, max_level: !args.max_level })
+      maxLevel: JSON.stringify({ ...payload, max_level: !args.max_level }),
+      equipmentOnly: JSON.stringify({ ...payload, equipment_only: !args.equipment_only })
     };
 
     const maxButtonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
       new ButtonBuilder()
         .setCustomId(customIds.maxLevel)
         .setLabel(args.max_level ? 'Town Hall Max Level' : 'Max Level')
+        .setStyle(ButtonStyle.Secondary),
+      new ButtonBuilder()
+        .setCustomId(customIds.equipmentOnly)
+        .setLabel(args.equipment_only ? 'Troops' : 'Equipment')
         .setStyle(ButtonStyle.Secondary)
     );
 
@@ -134,7 +135,7 @@ export default class UnitsCommand extends Command {
     });
   }
 
-  private embed(data: APIPlayer, showMaxLevel = false) {
+  private embed(data: APIPlayer, showMaxLevel = false, equipmentOnly = false) {
     const embed = new EmbedBuilder().setAuthor({ name: `${data.name} (${data.tag})` });
 
     const unlockedEquipment = unique([
@@ -188,7 +189,11 @@ export default class UnitsCommand extends Command {
       });
     }
 
-    for (const category of units.sort((a, b) => a.index - b.index)) {
+    const filteredUnits = units.filter((category) =>
+      equipmentOnly ? category.title === 'Equipment' : category.title !== 'Equipment'
+    );
+
+    for (const category of filteredUnits.sort((a, b) => a.index - b.index)) {
       const unitsArray = category.units.map((unit) => {
         const { maxLevel, level: _level } = apiTroops.find(
           (u) => u.name === unit.name && u.village === unit.village && u.type === unit.category
@@ -220,10 +225,10 @@ export default class UnitsCommand extends Command {
                       const unitIcon =
                         (unit.village === 'home' ? HOME_TROOPS : BUILDER_TROOPS)[unit.name] ||
                         unit.name;
-                      const level = this.padStart(unit.level);
+                      const level = padStart(unit.level, 2);
                       const maxLevel = showMaxLevel
-                        ? this.padEnd(unit.maxLevel)
-                        : this.padEnd(unit.hallMaxLevel);
+                        ? padEnd(unit.maxLevel, 2)
+                        : padEnd(unit.hallMaxLevel, 2);
                       return `${unitIcon} \`\u200e${level}/${maxLevel}\u200f\``;
                     })
                     .join(' ')
@@ -234,6 +239,8 @@ export default class UnitsCommand extends Command {
         });
       }
     }
+
+    if (equipmentOnly) return embed;
 
     const superTroops = RAW_SUPER_TROOPS.filter((unit) =>
       apiTroops.find(
@@ -282,10 +289,10 @@ export default class UnitsCommand extends Command {
                 chunks
                   .map((unit) => {
                     const unitIcon = SUPER_TROOPS[unit.name] || unit.name;
-                    const level = this.padStart(unit.level);
+                    const level = padStart(unit.level, 2);
                     const maxLevel = showMaxLevel
-                      ? this.padEnd(unit.maxLevel)
-                      : this.padEnd(unit.hallMaxLevel);
+                      ? padEnd(unit.maxLevel, 2)
+                      : padEnd(unit.hallMaxLevel, 2);
                     return `${unitIcon} \`\u200e${level}/${maxLevel}\u200f\``;
                   })
                   .join(' ')
@@ -297,13 +304,5 @@ export default class UnitsCommand extends Command {
     }
 
     return embed;
-  }
-
-  private padEnd(num: number) {
-    return num.toString().padEnd(2, ' ');
-  }
-
-  private padStart(num: number) {
-    return num.toString().padStart(2, ' ');
   }
 }
