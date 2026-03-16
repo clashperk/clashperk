f6afbe01870f46aa26e69714871cd4ec2ad675d1
suvajit <suvajit.me@gmail.com>
2026-03-16 14:11:06 +0530

    fix: /export clans

--- src/commands/export/export-clans.ts
+++ src/commands/export/export-clans.ts
@@ -1,7 +1,7 @@
 import { SheetType } from '@app/entities';
 import { CommandInteraction } from 'discord.js';
 import { Command } from '../../lib/handlers.js';
-import { CreateGoogleSheet } from '../../struct/google.js';
+import { CreateGoogleSheet, createHyperlink } from '../../struct/google.js';
 import { getExportComponents } from '../../util/helper.js';
 
 export default class ExportClansCommand extends Command {
@@ -15,11 +15,23 @@ export default class ExportClansCommand extends Command {
   }
 
   public async exec(interaction: CommandInteraction<'cached'>) {
-    const clans = await this.client.storage.find(interaction.guild.id);
+    const [clans, categories] = await Promise.all([
+      this.client.storage.find(interaction.guild.id),
+      this.client.storage.getOrCreateDefaultCategories(interaction.guild.id)
+    ]);
+    const clanStoresMap = Object.fromEntries(
+      clans.map((clan) => [clan.tag, clan.categoryId?.toHexString()])
+    );
+    const categoriesMap = Object.fromEntries(
+      categories.map((category) => [category.value, category.name])
+    );
+
+    const __clans = await this.client.redis.getClans(clans.map((clan) => clan.tag));
 
     const chunks = [];
-    const __clans = await this.client.redis.getClans(clans.map((clan) => clan.tag));
+
     for (const clan of __clans) {
+      const leader = clan.memberList.find((member) => member.role === 'leader');
       chunks.push({
         name: clan.name,
         tag: clan.tag,
@@ -28,11 +40,16 @@ export default class ExportClansCommand extends Command {
         location: clan.location?.name ?? '',
         members: clan.members,
         warLeague: clan.warLeague?.name ?? 'Unranked',
-        capitalLeague: clan.capitalLeague?.name ?? 'Unranked'
+        capitalLeague: clan.capitalLeague?.name ?? 'Unranked',
+        category: categoriesMap[clanStoresMap[clan.tag]!],
+        clanLeader: leader?.name,
+        clanLink: `https://link.clashofclans.com/?action=openclanprofile&tag=${clan.tag.slice(1)}`
       });
     }
-    if (!chunks.length)
+
+    if (!chunks.length) {
       return interaction.editReply(this.i18n('common.no_data', { lng: interaction.locale }));
+    }
 
     const sheets: CreateGoogleSheet[] = [
       {
@@ -44,7 +61,10 @@ export default class ExportClansCommand extends Command {
           { name: 'War Log', width: 100, align: 'LEFT' },
           { name: 'Location', width: 160, align: 'LEFT' },
           { name: 'War League', width: 160, align: 'LEFT' },
-          { name: 'Capital League', width: 160, align: 'LEFT' }
+          { name: 'Capital League', width: 160, align: 'LEFT' },
+          { name: 'Category', width: 160, align: 'LEFT' },
+          { name: 'Clan Leader', width: 160, align: 'LEFT' },
+          { name: 'Clan Link', width: 160, align: 'LEFT' }
         ],
         rows: chunks.map((chunk) => [
           chunk.name,
@@ -54,7 +74,10 @@ export default class ExportClansCommand extends Command {
           chunk.warLog,
           chunk.location,
           chunk.warLeague,
-          chunk.capitalLeague
+          chunk.capitalLeague,
+          chunk.category,
+          chunk.clanLeader,
+          createHyperlink(chunk.clanLink, 'Open in Game')
         ]),
         title: `Clans`
       }
