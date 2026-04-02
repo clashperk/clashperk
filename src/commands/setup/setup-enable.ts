51d1988e1490de698b2581590fd1f63a57dc277b
suvajit <suvajit.me@gmail.com>
2026-04-03 00:54:38 +0530

    chore: upgrade sentry

--- src/commands/setup/setup-enable.ts
+++ src/commands/setup/setup-enable.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, MessageFlags } from 'discord.js';
 import { Command } from '../../lib/handlers.js';
 
 export default class SetupEnableCommand extends Command {
@@ -14,7 +14,7 @@ export default class SetupEnableCommand extends Command {
   public exec(interaction: CommandInteraction<'cached'>) {
     return interaction.reply({
       content: `This command has been replaced with ${this.client.commands.get('/setup clan')} and ${this.client.commands.get('/setup clan-embed')}`,
-      ephemeral: true
+      flags: MessageFlags.Ephemeral
     });
   }
 }
