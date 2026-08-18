5ddcc8324658b749f8799fc0114e141c588621c6
suvajit <suvajit.me@gmail.com>
2026-08-18 10:08:45 +0530

    fix: prepare linux/amd64 build

--- scripts/update_dead_guilds.ts
+++ scripts/update_dead_guilds.ts
@@ -9,16 +9,24 @@ import { mongoClient } from '../src/struct/database.js';
   const stores = mongoClient.db('clashperk').collection<ClanStoresEntity>(Collections.CLAN_STORES);
   const guilds = mongoClient
     .db('clashperk')
-    .collection<{ guild: string; updatedAt: Date }>(Collections.BOT_GUILDS);
+    .collection<{ guild: string; updatedAt: Date; name: string }>(Collections.BOT_GUILDS);
 
   for await (const guild of guilds.find()) {
-    const isDead = guild.updatedAt.getTime() <= Date.now() - 365 * 24 * 60 * 60 * 1000;
+    const isDead =
+      !guild.updatedAt || guild.updatedAt.getTime() <= Date.now() - 365 * 24 * 60 * 60 * 1000;
+    let modified = false;
     if (isDead) {
-      await stores.updateMany(
+      const { modifiedCount } = await stores.updateMany(
         { guild: guild.guild, patron: false },
-        { $set: { lastExecution: guild.updatedAt } }
+        { $set: { lastExecution: guild.updatedAt || guild._id.getTimestamp() } }
       );
+      modified = !!modifiedCount;
     }
+    if (modified)
+      console.log(
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        `Disabling guild ${guild.name} (${guild.guild}) - ${guild.updatedAt || guild._id.getTimestamp()}`
+      );
   }
 
   return mongoClient.close();
